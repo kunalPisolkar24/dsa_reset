5fb39cbed2a21f0993a9ae66b8b1ea3502687fbb
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-08 17:57:36 +0530

    done with 4th problem

--- Graphs/Traversals/DFS/04. Count Connected Components/Solution.cpp
+++ Graphs/Traversals/DFS/04. Count Connected Components/Solution.cpp
@@ -1,12 +1,33 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int countComponents(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int countComponents(int n, vector<vector<int>> &edges) {
+    int count = 0;
+    vector<bool> vis(n, false);
+    vector<vector<int>> graph(n);
+
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    function<void(int)> dfs = [&](int src) -> void {
+      vis[src] = true;
+      for (auto v : graph[src]) {
+        if (!vis[v]) {
+          dfs(v);
+        }
+      }
+    };
+
+    for (int i = 0; i < n; i++) {
+      if (!vis[i]) {
+        count++;
+        dfs(i);
+      }
+    }
+    return count;
+  }
 };
