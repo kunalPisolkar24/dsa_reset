8f9166c2fdc8ba7d4f6aff03ef008bd4c80c91f6
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-08 17:25:21 +0530

    done 2nd problem

--- Graphs/Traversals/DFS/02. Count Reachable Nodes/Solution.cpp
+++ Graphs/Traversals/DFS/02. Count Reachable Nodes/Solution.cpp
@@ -1,12 +1,27 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int countReachable(int n, vector<vector<int>>& edges, int start) {
-        // Write your code here
-        return 0;
+  int countReachable(int n, vector<vector<int>> &edges, int start) {
+    int count = 0;
+    vector<vector<int>> graph(n + 1);
+    for(auto e: edges) {
+        graph[e[0]].push_back(e[1]);
+        graph[e[1]].push_back(e[0]);
     }
+    vector<bool> vis(n, false);
+
+    function<void(int)> dfs = [&](int src) -> void {
+        vis[src] = true;
+        count++;
+        for(auto v: graph[src]) {
+            if(!vis[v]) 
+                dfs(v);
+        }
+    };
+    
+    dfs(start);
+    return count;
+  }
 };
