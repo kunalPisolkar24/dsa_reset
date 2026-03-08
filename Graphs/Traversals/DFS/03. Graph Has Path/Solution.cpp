27ccb716d564d39b3746473225b55886b4cb7060
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-08 17:45:31 +0530

    done with 3rd

--- Graphs/Traversals/DFS/03. Graph Has Path/Solution.cpp
+++ Graphs/Traversals/DFS/03. Graph Has Path/Solution.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    bool hasPath(int n, vector<vector<int>>& edges, int u, int v) {
-        // Write your code here
-        return false;
+  bool hasPath(int n, vector<vector<int>> &edges, int u, int v) {
+    vector<vector<int>> graph(n + 1);
+    vector<bool> vis(n, false);
+
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    bool f = false;
+    function<void(int)> dfs = [&](int src) -> void {
+      vis[src] = true;
+      if (src == v)
+        f = true;
+      for (auto v : graph[src]) {
+        if (!vis[v])
+          dfs(v);
+      }
+    };
+
+    dfs(u);
+
+    return f;
+  }
 };
