a62c279b4b48583dc189ac3befe5db6f5ee1deab
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-08 18:39:41 +0530

    done with 4th

--- Graphs/Traversals/DFS/05. Component Sizes/Solution.cpp
+++ Graphs/Traversals/DFS/05. Component Sizes/Solution.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    vector<int> getComponentSizes(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+  vector<int> getComponentSizes(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n + 1);
+    vector<bool> vis(n, false);
+
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    vector<int> res;
+    function<int(int)> dfs = [&](int src) -> int {
+    int count = 1;
+      vis[src] = true;
+      for (auto v : graph[src]) {
+        if (!vis[v]) {
+            count += dfs(v);
+        }
+      }
+      return count;
+    };
+
+    for (int i = 0; i < n; i++) {
+      if (!vis[i]) {
+        res.push_back(dfs(i));
+      }
+    }
+
+    sort(res.begin(), res.end());
+
+    return res;
+  }
 };
