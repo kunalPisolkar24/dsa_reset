185eff689aa4e87801a1accd3c52d887a0889ec6
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 12:56:23 +0530

    done 3rd

--- Graphs/Traversals/BFS/03. Largest Component Size/Solution.cpp
+++ Graphs/Traversals/BFS/03. Largest Component Size/Solution.cpp
@@ -1,12 +1,44 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int largestComponentSize(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int largestComponentSize(int n, vector<vector<int>> &edges) {
+    int mx = 0;
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+    vector<bool> vis(n, false);
+
+    auto bfs = [&](int src) -> int {
+      int count = 0;
+      queue<int> q;
+      q.push(src);
+      vis[src] = true;
+
+      while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        count++;
+
+        for (auto v : graph[u]) {
+          if (!vis[v]) {
+            vis[v] = true;
+            q.push(v);
+          }
+        }
+      }
+      return count;
+    };
+
+    for (int i = 0; i < n; i++) {
+      if (!vis[i]) {
+        mx = max(mx, bfs(i));
+      }
+    }
+
+    return mx;
+  }
 };
