a4a68bcac6de35f4de9f51cfaf898e16d106bcd7
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 12:47:40 +0530

    done 1st

--- Graphs/Traversals/BFS/01. Print BFS Traversal/Solution.cpp
+++ Graphs/Traversals/BFS/01. Print BFS Traversal/Solution.cpp
@@ -1,12 +1,46 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
     vector<int> bfsTraversal(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+        vector<vector<int>> graph(n);
+        for(auto e: edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            sort(graph[i].begin(), graph[i].end());
+        }
+
+        vector<int> res; 
+
+        vector<bool> vis(n, false);
+        auto bfs = [&](int src) -> void {
+            queue<int> q;
+            q.push(src);
+            vis[src] = true;
+
+            while(!q.empty()) {
+                int u = q.front();
+                q.pop();
+                res.push_back(u);
+
+                for(auto v: graph[u]) {
+                    if(!vis[v]) {
+                        vis[v] = true;
+                        q.push(v);
+                    }
+                }
+            }
+        };
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                bfs(i);
+            }
+        }
+        return res;
     }
 };
