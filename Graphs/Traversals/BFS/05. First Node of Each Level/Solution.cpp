473c7dc723dbd4d3041a011b8845f3d0e6e98d82
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 14:22:37 +0530

    done 5th

--- Graphs/Traversals/BFS/05. First Node of Each Level/Solution.cpp
+++ Graphs/Traversals/BFS/05. First Node of Each Level/Solution.cpp
@@ -1,12 +1,46 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    vector<int> firstNodePerLevel(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+  vector<int> firstNodePerLevel(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    for (int i = 0; i < n; i++) {
+      sort(graph[i].begin(), graph[i].end());
+    }
+
+    vector<bool> vis(n, false);
+    vector<int> res;
+    auto bfs = [&](int src) -> void {
+      queue<int> q;
+      q.push(src);
+      vis[src] = true;
+
+      while (!q.empty()) {
+        int len = q.size(), f = 0;
+        for (int lev = 0; lev < len; lev++) {
+          int u = q.front();
+          q.pop();
+          if (!f) {
+            res.push_back(u);
+            f = true;
+          }
+          for (auto v : graph[u]) {
+            if (!vis[v]) {
+              vis[v] = true;
+              q.push(v);
+            }
+          }
+        }
+      }
+    };
+
+    bfs(0);
+    return res;
+  }
 };
