bdf0d8a85ed23463e4bf14a994dff30dc7948cce
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 14:40:14 +0530

    done 7th

--- Graphs/Traversals/BFS/07. Distance From Source/Solution.cpp
+++ Graphs/Traversals/BFS/07. Distance From Source/Solution.cpp
@@ -1,12 +1,38 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    vector<int> distanceFromSource(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+  vector<int> distanceFromSource(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    vector<int> dist(n, -1);
+    auto bfs = [&](int src) -> void {
+      queue<int> q;
+      q.push(src);
+      dist[src] = 0;
+
+      while (!q.empty()) {
+        int len = q.size();
+        for (int i = 0; i < len; i++) {
+          int u = q.front();
+          q.pop();
+
+          for (auto v : graph[u]) {
+            if (dist[v] == -1) {
+              dist[v] = dist[u] + 1;
+              q.push(v);
+            }
+          }
+        }
+      }
+    };
+
+    bfs(0);
+    return dist;
+  }
 };
