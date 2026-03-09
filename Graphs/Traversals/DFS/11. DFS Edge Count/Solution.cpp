19dabfe22b4286b31a47ae984fe3de0b63bff4f4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 09:53:29 +0530

    done w/ 11th

--- Graphs/Traversals/DFS/11. DFS Edge Count/Solution.cpp
+++ Graphs/Traversals/DFS/11. DFS Edge Count/Solution.cpp
@@ -1,12 +1,36 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int countTreeEdges(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int countTreeEdges(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+    for (int i = 0; i < n; i++) {
+      sort(graph[i].begin(), graph[i].end());
+    }
+
+    vector<bool> vis(n, false);
+    int count = 0;
+    function<void(int)> dfs = [&](int src) -> void {
+        vis[src] = true;
+        for(auto v: graph[src]) {
+            if(!vis[v]) {
+                count++;
+                dfs(v);
+            }
+        }
+    };
+
+    for(int i = 0; i < n; i++) {
+        if(!vis[i]) {
+            dfs(i);
+        }
+    }
+
+    return count;
+  }
 };
