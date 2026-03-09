8e2e7d03c9815096c25b2761471b0fc26b400a66
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 09:19:51 +0530

    done w/ 7th

--- Graphs/Traversals/DFS/07. Count Leaf Nodes in DFS Tree/Solution.cpp
+++ Graphs/Traversals/DFS/07. Count Leaf Nodes in DFS Tree/Solution.cpp
@@ -1,12 +1,36 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int countLeafNodes(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int countLeafNodes(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n + 1);
+    vector<bool> vis(n, false);
+
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    for (int i = 0; i < n; i++)
+      sort(graph[i].begin(), graph[i].end());
+
+    int count = 0;
+    function<void(int)> dfs = [&](int src) -> void {
+      vis[src] = true;
+      bool f = false;
+      for (auto v : graph[src]) {
+        if (!vis[v]) {
+          f = true;
+          dfs(v);
+        }
+      }
+      if (f == false)
+        count++;
+    };
+
+    dfs(0);
+
+    return count;
+  }
 };
