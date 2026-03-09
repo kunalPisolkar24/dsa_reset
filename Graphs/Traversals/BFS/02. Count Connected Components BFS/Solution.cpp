c4c868e5d510820b632bad87b08534a9872fb1c0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 12:51:05 +0530

    done 2nd

--- Graphs/Traversals/BFS/02. Count Connected Components BFS/Solution.cpp
+++ Graphs/Traversals/BFS/02. Count Connected Components BFS/Solution.cpp
@@ -1,12 +1,43 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int countComponentsBFS(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int countComponentsBFS(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    vector<bool> vis(n, false);
+    int count = 0;
+
+    auto bfs = [&](int src) -> void {
+      queue<int> q;
+      q.push(src);
+      vis[src] = true;
+
+      while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (auto v : graph[u]) {
+          if (!vis[v]) {
+            vis[v] = true;
+            q.push(v);
+          }
+        }
+      }
+    };
+
+    for (int i = 0; i < n; i++) {
+      if (!vis[i]) {
+        count++;
+        bfs(i);
+      }
+    }
+
+    return count;
+  }
 };
