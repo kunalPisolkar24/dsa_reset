c04b5ec2f9774455da4d5395f174b79c22ed44f9
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 19:16:19 +0530

    done with 12th

--- Graphs/Traversals/BFS/12. Maximum BFS Width/Solution.cpp
+++ Graphs/Traversals/BFS/12. Maximum BFS Width/Solution.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int maxBFSWidth(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return 0;
+  int maxBFSWidth(int n, vector<vector<int>> &edges) {
+    int mx = numeric_limits<int>::min();
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+    vector<bool> vis(n, false);
+    auto bfs = [&](int src) -> void {
+      queue<int> q;
+      q.push(src);
+      vis[src] = true;
+      while (!q.empty()) {
+        int len = q.size();
+        mx = max(mx, len);
+        for (int i = 0; i < len; i++) {
+          int u = q.front();
+          q.pop();
+
+          for (auto v : graph[u]) {
+            if (!vis[v]) {
+              vis[v] = true;
+              q.push(v);
+            }
+          }
+        }
+      }
+    };
+    bfs(0);
+    return mx;
+  }
 };
