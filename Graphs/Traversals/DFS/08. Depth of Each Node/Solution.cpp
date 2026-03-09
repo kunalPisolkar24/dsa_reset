b1055b912e169354cb3bd4c7eee79f730d0ca430
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 09:33:04 +0530

    done 8th

--- Graphs/Traversals/DFS/08. Depth of Each Node/Solution.cpp
+++ Graphs/Traversals/DFS/08. Depth of Each Node/Solution.cpp
@@ -1,12 +1,34 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    vector<int> getDepths(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+  vector<int> getDepths(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n + 1);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+    for(int i = 0; i < n; i++) 
+        sort(graph[i].begin(), graph[i].end());
+    
+    vector<int> dist(n, -1);
+
+    function<void(int)> dfs = [&](int src) -> void {
+      for (auto v : graph[src]) {
+        if (dist[v] == -1) {
+          dist[v] = dist[src] + 1;
+          dfs(v);
+        }
+      }
+    };
+
+    for (int i = 0; i < n; i++) {
+      if (dist[i] == -1) {
+        dist[i] = 0;
+        dfs(i);
+      }
+    }
+    return dist;
+  }
 };
