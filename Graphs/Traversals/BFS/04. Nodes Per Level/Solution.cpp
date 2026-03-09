98711f4ed8fbcb8f835669258d519b626a0478ec
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-09 14:14:15 +0530

    done 4th

--- Graphs/Traversals/BFS/04. Nodes Per Level/Solution.cpp
+++ Graphs/Traversals/BFS/04. Nodes Per Level/Solution.cpp
@@ -1,12 +1,38 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    vector<int> nodesPerLevel(int n, vector<vector<int>>& edges) {
-        // Write your code here
-        return {};
+  vector<int> nodesPerLevel(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+
+    vector<int> res;
+    vector<bool> vis(n, false);
+    auto bfs = [&](int src) -> void {
+      queue<int> q;
+      q.push(src);
+      vis[src] = 1;
+      while (!q.empty()) {
+        int len = q.size();
+        for (int lev = 0; lev < len; lev++) {
+          int u = q.front();
+          q.pop();
+
+          for (auto v : graph[u]) {
+            if (!vis[v]) {
+              vis[v] = true;
+              q.push(v);
+            }
+          }
+        }
+        res.push_back(len);
+      }
+    };
+    bfs(0);
+    return res;
+  }
 };
