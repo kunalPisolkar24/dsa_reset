5c263d860e8ebc74186ff128837e3b0b86e774da
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-10 19:02:03 +0530

    done with 4th

--- Graphs/Grids/04. Island Perimeter/Solution.cpp
+++ Graphs/Grids/04. Island Perimeter/Solution.cpp
@@ -4,8 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        // Write your code here
+  int islandPerimeter(vector<vector<int>> &grid) {
+    int count = 0;
+    int n = grid.size(), m = grid[0].size();
+    function<int(int, int)> dfs = [&](int i, int j) -> int {
+      int count = 0;
+      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0)
+        return 1;
+      if (grid[i][j] == 2)
         return 0;
+      grid[i][j] = 2;
+      count += dfs(i + 1, j);
+      count += dfs(i - 1, j);
+      count += dfs(i, j + 1);
+      count += dfs(i, j - 1);
+      return count;
+    };
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 1) {
+          count += dfs(i, j);
+        }
+      }
     }
+    return count;
+  }
 };
