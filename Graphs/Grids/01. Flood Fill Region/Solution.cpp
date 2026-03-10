3777450256fcc784ce70e3bec4ef2274a8180f28
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-10 18:22:34 +0530

    done with 1st

--- Graphs/Grids/01. Flood Fill Region/Solution.cpp
+++ Graphs/Grids/01. Flood Fill Region/Solution.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int r, int c, int newColor) {
-        // Write your code here
-        return {};
-    }
+  vector<vector<int>> floodFill(vector<vector<int>> &grid, int r, int c,
+                                int newColor) {
+
+    int n = grid.size(), m = grid[0].size();
+    int required = grid[r][c];
+
+    function<void(int, int)> sol = [&](int i, int j) -> void {
+      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] != required)
+        return;
+      grid[i][j] = newColor;
+      sol(i + 1, j);
+      sol(i - 1, j);
+      sol(i, j + 1);
+      sol(i, j - 1);
+    };
+    sol(r, c);
+    return grid;
+  }
 };
