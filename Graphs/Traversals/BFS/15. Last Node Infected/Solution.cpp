942b112a8071a5ec777738297fa941a176eb2b22
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-10 10:49:53 +0530

    done 15th

--- Graphs/Traversals/BFS/15. Last Node Infected/Solution.cpp
+++ Graphs/Traversals/BFS/15. Last Node Infected/Solution.cpp
@@ -1,12 +1,44 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 class Solution {
 public:
-    int lastNodeInfected(int n, vector<vector<int>>& edges, vector<int>& sources) {
-        // Write your code here
-        return 0;
+  int lastNodeInfected(int n, vector<vector<int>> &edges,
+                       vector<int> &sources) {
+    int mxDist = numeric_limits<int>::min(), mxEl = -1;
+    vector<vector<int>> graph(n);
+    for (auto e : edges) {
+      graph[e[0]].push_back(e[1]);
+      graph[e[1]].push_back(e[0]);
     }
+    vector<int> dist(n, -1);
+    auto bfs = [&]() {
+      queue<int> q;
+      for (auto i : sources) {
+        q.push(i);
+        dist[i] = 0;
+      }
+
+      while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (auto v : graph[u]) {
+          if (dist[v] == -1) {
+            dist[v] = dist[u] + 1;
+            q.push(v);
+          }
+        }
+      }
+    };
+
+    bfs();
+    for (int i = 0; i < n; i++) {
+      if (mxDist < dist[i]) {
+        mxDist = dist[i];
+        mxEl = i;
+      }
+    }
+    return mxEl;
+  }
 };
