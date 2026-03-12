301271c03ebd6d4c1238dfc235e33011c3eb4ada
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-12 18:38:58 +0530

    done with 1st

--- Graphs/Grids/Boundary Traversing/01. Boundary Flood Fill/Solution.cpp
+++ Graphs/Grids/Boundary Traversing/01. Boundary Flood Fill/Solution.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> boundaryFloodFill(vector<vector<int>>& grid) {
-        // Your code here
-        return grid;
+  vector<vector<int>> boundaryFloodFill(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+
+    function<void(int, int)> dfs = [&](int i, int j) -> void {
+      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0 or
+          grid[i][j] == 2)
+        return;
+      grid[i][j] = 2;
+      dfs(i + 1, j);
+      dfs(i - 1, j);
+      dfs(i, j - 1);
+      dfs(i, j + 1);
+    };
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i == 0 or j == 0 or i == n - 1 or j == m - 1) {
+          if (grid[i][j])
+            dfs(i, j);
+        }
+      }
     }
+
+    return grid;
+  }
 };
