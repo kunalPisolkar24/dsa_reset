4d48f851e8f36ad6039b3b5c0cedfb8645d425a8
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-12 19:53:02 +0530

    done 3rd

--- Graphs/Grids/03. Largest Island Size/Solution.cpp
+++ Graphs/Grids/03. Largest Island Size/Solution.cpp
@@ -7,22 +7,37 @@ public:
   int largestIsland(vector<vector<int>> &grid) {
     int n = grid.size(), m = grid[0].size();
     int mxArea = numeric_limits<int>::min();
-    function<int(int,int)> dfs = [&](int i, int j) -> int {
-        int count = 1;
-        if(i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0)
-            return 0;
-        grid[i][j] = 0;
-        count += dfs(i - 1, j);
-        count += dfs(i + 1, j);
-        count += dfs(i, j - 1);
-        count += dfs(i, j + 1);
-        return count;
+
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+
+    auto bfs = [&](int row, int col) -> int {
+      int count = 0;
+      queue<pair<int, int>> q;
+      q.push({row, col});
+      grid[row][col] = 2;
+
+      while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        count++;
+
+        for (int i = 0; i < 4; i++) {
+          int nx = x + dx[i];
+          int ny = y + dy[i];
+
+          if (nx >= 0 and ny >= 0 and nx < n and ny < m and grid[nx][ny] == 1) {
+            grid[nx][ny] = 2;
+            q.push({nx, ny});
+          }
+        }
+      }
+      return count;
     };
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        if (grid[i][j]) {
-          int area = dfs(i, j);
+        if (grid[i][j] == 1) {
+          int area = bfs(i, j);
           mxArea = max(mxArea, area);
         }
       }
