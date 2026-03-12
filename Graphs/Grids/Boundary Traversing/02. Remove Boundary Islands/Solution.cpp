ae256c6b5a57c18c4eb0de1191684e61317b1e81
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-12 19:08:25 +0530

    done with 2nd

--- Graphs/Grids/Boundary Traversing/02. Remove Boundary Islands/Solution.cpp
+++ Graphs/Grids/Boundary Traversing/02. Remove Boundary Islands/Solution.cpp
@@ -3,8 +3,28 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> removeBoundaryIslands(vector<vector<int>>& grid) {
-        // Your code here
-        return grid;
+  vector<vector<int>> removeBoundaryIslands(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+
+    function<void(int, int)> dfs = [&](int i, int j) -> void {
+      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0)
+        return;
+      grid[i][j] = 0;
+      dfs(i + 1, j);
+      dfs(i - 1, j);
+      dfs(i, j - 1);
+      dfs(i, j + 1);
+    };
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i == 0 or j == 0 or i == n - 1 or j == m - 1) {
+          if (grid[i][j]) {
+            dfs(i, j);
+          }
+        }
+      }
     }
+    return grid;
+  }
 };
