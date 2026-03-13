d7b2d2073055d0e6046a09f724ff66f312944cd0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-13 12:51:22 +0530

    done 1st

--- Graphs/Grids/Boundary Traversing/01. Boundary Flood Fill/Solution.cpp
+++ Graphs/Grids/Boundary Traversing/01. Boundary Flood Fill/Solution.cpp
@@ -6,22 +6,36 @@ public:
   vector<vector<int>> boundaryFloodFill(vector<vector<int>> &grid) {
     int n = grid.size(), m = grid[0].size();
 
-    function<void(int, int)> dfs = [&](int i, int j) -> void {
-      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0 or
-          grid[i][j] == 2)
-        return;
-      grid[i][j] = 2;
-      dfs(i + 1, j);
-      dfs(i - 1, j);
-      dfs(i, j - 1);
-      dfs(i, j + 1);
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+    auto bfs = [&](int row, int col) -> void {
+      queue<pair<int, int>> q;
+      q.push({row, col});
+      grid[row][col] = 2;
+
+      while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+          int nx = x + dx[i];
+          int ny = y + dy[i];
+
+          bool bounds = nx >= 0 and ny >= 0 and nx < n and ny < m;
+          if (bounds) {
+            if (grid[nx][ny] == 1) {
+              grid[nx][ny] = 2;
+              q.push({nx, ny});
+            }
+          }
+        }
+      }
     };
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (i == 0 or j == 0 or i == n - 1 or j == m - 1) {
           if (grid[i][j])
-            dfs(i, j);
+            bfs(i, j);
         }
       }
     }
