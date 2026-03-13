05a6a6294d5da7131490453d898dca8acc7d9df4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-13 15:09:56 +0530

    done with 8th

--- Graphs/Grids/08. Multi-Source Distance/Solution.cpp
+++ Graphs/Grids/08. Multi-Source Distance/Solution.cpp
@@ -4,8 +4,37 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> multiSourceDistance(vector<vector<int>>& grid) {
-        // Write your code here
-        return {};
+  vector<vector<int>> multiSourceDistance(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+    queue<pair<int, int>> q;
+    vector<vector<int>> dist(n, vector<int>(m, -1));
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 1) {
+          q.push({i, j});
+          dist[i][j] = 0;
+        }
+      }
     }
+
+    while (!q.empty()) {
+      auto [x, y] = q.front();
+      q.pop();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        bool bounds = nx >= 0 and ny >= 0 and nx < n and ny < m;
+        if (bounds) {
+          if (dist[nx][ny] == -1) {
+            dist[nx][ny] = 1 + dist[x][y];
+            q.push({nx, ny});
+          }
+        }
+      }
+    }
+    return dist;
+  }
 };
