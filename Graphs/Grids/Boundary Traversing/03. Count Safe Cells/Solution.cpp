8515015411c0c8957853b3e26d9fa8dc25280cb4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-13 13:16:18 +0530

    done with 3rd

--- Graphs/Grids/Boundary Traversing/03. Count Safe Cells/Solution.cpp
+++ Graphs/Grids/Boundary Traversing/03. Count Safe Cells/Solution.cpp
@@ -6,33 +6,49 @@ public:
   int countSafeCells(vector<vector<int>> &grid) {
     int n = grid.size(), m = grid[0].size();
 
-    function<void(int, int)> dfs = [&](int i, int j) -> void {
-      if (i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0)
-        return;
-      grid[i][j] = 0;
-      dfs(i - 1, j);
-      dfs(i + 1, j);
-      dfs(i, j - 1);
-      dfs(i, j + 1);
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+
+    auto bfs = [&](int row, int col) -> void {
+      queue<pair<int, int>> q;
+      q.push({row, col});
+      grid[row][col] = 0;
+
+      while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+          int nx = x + dx[i];
+          int ny = y + dy[i];
+
+          bool bounds = nx >= 0 and ny >= 0 and nx < n and ny < m;
+          if (bounds) {
+            if (grid[nx][ny] == 1) {
+              grid[nx][ny] = 0;
+              q.push({nx, ny});
+            }
+          }
+        }
+      }
     };
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (i == 0 or j == 0 or i == n - 1 or j == m - 1) {
           if (grid[i][j]) {
-            dfs(i, j);
+            bfs(i, j);
           }
         }
       }
     }
 
     int count = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(grid[i][j]) {
-                count++;
-            }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j]) {
+          count++;
         }
+      }
     }
 
     return count;
