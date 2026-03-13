c0fe16ea0390086735296a02e09988fb06ba48c9
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-13 13:53:55 +0530

    done with 6th

--- Graphs/Grids/06. Shortest Path in Grid/Solution.cpp
+++ Graphs/Grids/06. Shortest Path in Grid/Solution.cpp
@@ -4,8 +4,40 @@ using namespace std;
 
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid) {
-        // Write your code here
-        return 0;
+  int shortestPath(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+    if (grid[0][0] == 1)
+      return -1;
+
+    vector<vector<int>> dist(n, vector<int>(m, -1));
+
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    dist[0][0] = 0;
+
+    while (!q.empty()) {
+      int size = q.size();
+
+      while (size--) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+          int nx = x + dx[i];
+          int ny = y + dy[i];
+
+          bool bounds = nx >= 0 and ny >= 0 and nx < n and ny < m;
+          if (bounds) {
+            if (dist[nx][ny] == -1 and grid[nx][ny] != 1) {
+              dist[nx][ny] = dist[x][y] + 1;
+              q.push({nx, ny});
+            }
+          }
+        }
+      }
     }
+
+    return dist[n - 1][m - 1];
+  }
 };
