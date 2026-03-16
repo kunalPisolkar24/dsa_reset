c151d1dac445b7bcd613ae857214a9f71407b2b2
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 20:41:27 +0530

    done with 1st

--- BinarySearch/01. Basic Existence Search/Solution.cpp
+++ BinarySearch/01. Basic Existence Search/Solution.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        // TODO: Implement binary search to return the index of target, or -1 if not present.
-        return -1;
+  int search(vector<int> &nums, int target) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
     }
+    return -1;
+  }
 };
