7520f56a9a5e9e2f80b3c2442eb9b1f4ee79f2b0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 21:15:41 +0530

    done w/ 5th

--- BinarySearch/05. Insert Position/Solution.cpp
+++ BinarySearch/05. Insert Position/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        // TODO: Return the index where target should be inserted (first index i with nums[i] >= target).
-        return -1;
+  int searchInsert(vector<int> &nums, int target) {
+    int n = nums.size(), ans = n;
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] >= target) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
