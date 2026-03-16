0c357e3d3687685c219647f51a430bd6bf192bbe
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 20:59:17 +0530

    done with 4th

--- BinarySearch/04. Count Frequency/Solution.cpp
+++ BinarySearch/04. Count Frequency/Solution.cpp
@@ -4,18 +4,46 @@ using namespace std;
 
 class Solution {
 public:
-    int findFirst(vector<int>& nums, int target) {
-        // Helper to find the first occurrence
-        return -1;
+  int countFrequency(vector<int> &nums, int target) {
+    int n = nums.size();
+    auto first = [&]() -> int {
+      int ans = -1;
+      int low = 0, high = n - 1;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+          ans = mid;
+          high = mid - 1;
+        } else if (nums[mid] < target) {
+          low = mid + 1;
+        } else
+          high = mid - 1;
+      }
+      return ans;
+    };
+
+    auto last = [&]() -> int {
+      int ans = -1;
+      int low = 0, high = n - 1;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+          ans = mid;
+          low = mid + 1;
+        } else if (nums[mid] < target) {
+          low = mid + 1;
+        } else
+          high = mid - 1;
+      }
+      return ans;
+    };
+
+    int firstOcc = first();
+    int lastOcc = last();
+    if(firstOcc != -1 and lastOcc != -1) {
+        return (lastOcc - firstOcc) + 1;
     }
 
-    int findLast(vector<int>& nums, int target) {
-        // Helper to find the last occurrence
-        return -1;
-    }
-
-    int countFrequency(vector<int>& nums, int target) {
-        // TODO: Use findFirst and findLast to compute total frequency of target.
-        return 0;
-    }
+    return 0;
+  }
 };
