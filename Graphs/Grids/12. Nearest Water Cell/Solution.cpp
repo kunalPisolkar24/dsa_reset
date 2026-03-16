d6b6c7613069ed3ed478694daf3ee89b5a158a01
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 12:45:17 +0530

    done with 12

--- Graphs/Grids/12. Nearest Water Cell/Solution.cpp
+++ Graphs/Grids/12. Nearest Water Cell/Solution.cpp
@@ -4,8 +4,38 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> nearestWaterCell(vector<vector<int>>& grid) {
-        // Write your code here
-        return {};
+  vector<vector<int>> nearestWaterCell(vector<vector<int>> &grid) {
+    int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> dist(n, vector<int>(m, -1));
+
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 0) {
+          q.push({i, j});
+          dist[i][j] = 0;
+        }
+      }
     }
+    vector<int> dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+
+    while (!q.empty()) {
+      auto [x, y] = q.front();
+      q.pop();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        bool bounds = nx >= 0 and ny >= 0 and nx < n and ny < m;
+        if (bounds) {
+          if (dist[nx][ny] == -1) {
+            dist[nx][ny] = 1 + dist[x][y];
+            q.push({nx, ny});
+          }
+        }
+      }
+    }
+    return dist;
+  }
 };
