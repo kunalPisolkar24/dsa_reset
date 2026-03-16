10e015332bc71cc2f70d6f14eb9fe530e79a8274
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 20:52:14 +0530

    done with 3rd

--- BinarySearch/03. Last Occurrence/Solution.cpp
+++ BinarySearch/03. Last Occurrence/Solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        // TODO: Implement binary search to return the last occurrence of target, or -1 if absent.
-        return -1;
+  int search(vector<int> &nums, int target) {
+    int n = nums.size(), ans = -1;
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        ans = mid;
+        low = mid + 1;
+      } else if (nums[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
