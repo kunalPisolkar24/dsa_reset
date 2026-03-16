a8c7ae3b53efb875e415c3f7ba5e4f58b6664829
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-16 20:49:42 +0530

    done with 2nd

--- BinarySearch/02. First Occurrence/Solution.cpp
+++ BinarySearch/02. First Occurrence/Solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        // TODO: Implement binary search to return the first occurrence of target, or -1 if absent.
-        return -1;
+  int search(vector<int> &nums, int target) {
+    int ans = -1, n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        ans = mid;
+        high = mid - 1;
+      } else if (nums[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
