ad5c74c2a492bd953ee599466b64f3b30a335158
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 18:39:30 +0530

    done w/ 5

--- BinarySearch/Ranges/05. Minimum Eating Speed/Solution.cpp
+++ BinarySearch/Ranges/05. Minimum Eating Speed/Solution.cpp
@@ -1,11 +1,30 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        // Find minimum eating speed k such that all bananas can be eaten within h hours.
-        return 0;
+  int minEatingSpeed(vector<int> &piles, int h) {
+    int n = piles.size();
+    auto check = [&](int k) -> bool {
+      long long count = 0;
+      for (int i = 0; i < n; i++) {
+        count += (piles[i] + k - 1) / k;
+      }
+      return count <= h;
+    };
+
+    int low = 1, high = *max_element(piles.begin(), piles.end()), ans = 0;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+
+    return ans;
+  }
 };
