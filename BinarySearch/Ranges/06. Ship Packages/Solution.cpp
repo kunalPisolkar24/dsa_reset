4e3a6110ea4a8241f45579684c2ddd4d20c73540
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 19:18:37 +0530

    done with 6th

--- BinarySearch/Ranges/06. Ship Packages/Solution.cpp
+++ BinarySearch/Ranges/06. Ship Packages/Solution.cpp
@@ -4,8 +4,35 @@ using namespace std;
 
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        // Find minimum capacity of a ship to transport all weights within given days.
-        return 0;
+  int shipWithinDays(vector<int> &weights, int days) {
+    int n = weights.size();
+
+    auto check = [&](int cap) -> bool {
+      int sum = 0, count = 1;
+      for (int i = 0; i < n; i++) {
+
+        if (weights[i] > cap)
+          return false;
+
+        if (sum + weights[i] > cap) {
+          count++;
+          sum = 0;
+        }
+        sum += weights[i];
+      }
+      return count <= days;
+    };
+
+    int low = 1, high = accumulate(weights.begin(), weights.end(), 0),
+        ans = high;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
