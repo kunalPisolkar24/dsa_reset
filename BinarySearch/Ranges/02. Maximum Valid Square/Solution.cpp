f47b1a877f2b44c402c69ec0e1819ff269f98c40
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 17:10:51 +0530

    done w/ 2nd

--- BinarySearch/Ranges/02. Maximum Valid Square/Solution.cpp
+++ BinarySearch/Ranges/02. Maximum Valid Square/Solution.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    long long maxValidSquare(long long x) {
-        // Find largest y such that y*y <= x.
-        // Use binary search on y.
-        return 0;
+  long long maxValidSquare(long long x) {
+    long long low = 0, high = x, ans = x;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      long long val = mid * mid;
+      if (val <= x) {
+        ans = mid;
+        low = mid + 1;
+      } else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
