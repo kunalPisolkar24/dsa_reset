0a3e473f79f7cf3ec0ecec94662cc391c125ab06
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 17:35:30 +0530

    done w/ 3rd

--- BinarySearch/Ranges/03. Minimum Divisor/Solution.cpp
+++ BinarySearch/Ranges/03. Minimum Divisor/Solution.cpp
@@ -1,11 +1,30 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    int smallestDivisor(vector<int>& nums, int limit) {
-        // Find smallest divisor d such that sum(ceil(nums[i] / d)) <= limit.
-        return 0;
+  int smallestDivisor(vector<int> &nums, int limit) {
+    int n = nums.size();
+
+    auto sol = [&](int d) -> int {
+      int sum = 0;
+      for (int i = 0; i < n; i++) {
+        sum += (nums[i] + d - 1) / d;
+      }
+      return sum;
+    };
+
+    int low = 1, high = *max_element(nums.begin(), nums.end()), ans = 0;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (sol(mid) <= limit) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
