db5a030cc9685bd73c1f42b218dfb8cc25e66946
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 17:08:23 +0530

    done w/ 1st

--- BinarySearch/Ranges/01. Minimum Square Threshold/Solution.cpp
+++ BinarySearch/Ranges/01. Minimum Square Threshold/Solution.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    long long minSquareThreshold(long long x) {
-        // Find smallest y such that y*y >= x.
-        // Use binary search on y in range [1, x].
-        return 0;
+  long long minSquareThreshold(long long x) {
+    long long low = 0, high = x, ans = high;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      long long int val = mid * mid;
+      if(val >= x) {
+        ans = mid;
+        high = mid - 1;
+      } else low = mid + 1;
     }
+    return ans;
+  }
 };
