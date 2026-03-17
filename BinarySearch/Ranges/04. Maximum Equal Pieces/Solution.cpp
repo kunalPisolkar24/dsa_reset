5b9b1ab0fd269585511bec51dcd5b1bd57f12c8e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 18:13:23 +0530

    done w/ 4th

--- BinarySearch/Ranges/04. Maximum Equal Pieces/Solution.cpp
+++ BinarySearch/Ranges/04. Maximum Equal Pieces/Solution.cpp
@@ -4,8 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    int maxPieceLength(vector<int>& nums, int k) {
-        // Find maximum length of pieces such that at least k pieces can be cut from ropes in nums.
-        return 0;
+  int maxPieceLength(vector<int> &nums, int k) {
+    int n = nums.size();
+
+    auto check = [&](int length) -> bool {
+      long long count = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        count += nums[i] / length;
+        if(count >= k) return true;
+      }
+      return false;
+    };
+
+    int low = 1, high = *max_element(nums.begin(), nums.end()), ans = 0;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        low = mid + 1;
+      } else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
