e2d802189f0f16741d76e39e8075ac781713d973
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 23:18:39 +0530

    done with 9th

--- BinarySearch/Ranges/09. Minimum Time to Complete Tasks/Solution.cpp
+++ BinarySearch/Ranges/09. Minimum Time to Complete Tasks/Solution.cpp
@@ -1,11 +1,30 @@
+#include <algorithm>
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
 public:
-    long long minTimeToCompleteTasks(vector<int>& time, int k) {
-        // Find minimum time for k tasks to be completed if each worker works at their own speed in parallel.
-        return 0;
+  long long minTimeToCompleteTasks(vector<int> &times, int k) {
+    int n = times.size();
+
+    auto check = [&](long long time) -> bool {
+      long long total = 0;
+      for (int i = 0; i < n; i++) {
+        total += (time / times[i]);
+      }
+      return total >= k;
+    };
+
+    long long low = 1, high = *max_element(times.begin(), times.end()) * k;
+    long long ans = 0;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
