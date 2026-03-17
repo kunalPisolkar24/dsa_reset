010a40344688d4db858a5cf9fc7c6c6b604d48e8
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 11:47:39 +0530

    done with 6th

--- BinarySearch/06. Floor and Ceil/Solution.cpp
+++ BinarySearch/06. Floor and Ceil/Solution.cpp
@@ -4,11 +4,44 @@ using namespace std;
 
 class Solution {
 public:
-    pair<int, int> findFloorCeil(vector<int>& nums, int target) {
-        // TODO: Return {floor, ceil}. 
-        // Floor: largest element <= target. 
-        // Ceil: smallest element >= target.
-        // Return -1 if not found.
-        return {-1, -1};
+  pair<int, int> findFloorCeil(vector<int> &nums, int target) {
+    int n = nums.size();
+
+    auto getCeil = [&]() -> int {
+      int low = 0, high = n - 1, ans = n;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] >= target) {
+          ans = mid;
+          high = mid - 1;
+        } else if (nums[mid] < target)
+          low = mid + 1;
+      }
+      return (ans == n ? -1 : ans);
+    };
+
+    auto getFloor = [&]() -> int {
+      int low = 0, high = n - 1, ans = n;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] <= target) {
+          ans = mid;
+          low = mid + 1;
+        } else
+          high = mid - 1;
+      }
+      return (ans == n ? -1 : ans);
+    };
+
+    int floorEl = getFloor(), ceilEl = getCeil();
+    if (floorEl != -1 and ceilEl != -1) {
+      return {nums[floorEl], nums[ceilEl]};
+    } else if (floorEl == -1 and ceilEl != -1) {
+      return {-1, nums[ceilEl]};
+    } else if (floorEl != -1 and ceilEl == -1) {
+      return {nums[floorEl], -1};
     }
+
+    return {-1, -1};
+  }
 };
