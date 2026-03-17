fb51fb0a9a171f2a90aeeef919e2fc90c5049efc
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-17 21:09:56 +0530

    done with 7th

--- BinarySearch/Ranges/07. Allocate Books/Solution.cpp
+++ BinarySearch/Ranges/07. Allocate Books/Solution.cpp
@@ -4,8 +4,33 @@ using namespace std;
 
 class Solution {
 public:
-    int allocateBooks(vector<int>& pages, int m) {
-        // Distribute books among m students such that the maximum pages assigned to a student is minimized.
-        return 0;
+  int allocateBooks(vector<int> &pages, int m) {
+    int n = pages.size();
+    if(m > n) return -1;
+    
+    auto check = [&](int cap) -> bool {
+      int count = 1;
+      long long sum = 0;
+      for (int i = 0; i < n; i++) {
+        if (sum + pages[i] > cap) {
+          count++;
+          sum = 0;
+        }
+        sum += pages[i];
+      }
+      return count <= m;
+    };
+
+    int low = *max_element(pages.begin(), pages.end()), high = accumulate(pages.begin(), pages.end(), 0);
+    int ans = -1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
