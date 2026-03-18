f144fa72e6b52df4bf5a1bab2b6e5c50f56b0bd3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-18 18:50:55 +0530

    done with 15th

--- BinarySearch/Ranges/15. First Failing Point/Solution.cpp
+++ BinarySearch/Ranges/15. First Failing Point/Solution.cpp
@@ -4,9 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    long long firstFailingPoint(long long low, long long high, vector<int>& thresholds, long long limit) {
-        // Find the smallest integer x in [low, high] such that sum(ceil(thresholds[i] / x)) <= limit.
-        // Use the "First True" binary search pattern.
-        return 0;
+  long long firstFailingPoint(long long low, long long high,
+                              vector<int> &thresholds, long long limit) {
+    int n = thresholds.size();
+
+    auto f = [&](long long x) -> bool {
+      long long sum = 0;
+      for (int i = 0; i < n; i++) {
+        sum += (long long)(thresholds[i] + x - 1) / x;
+      }
+      return sum <= limit;
+    };
+
+    long long ans = -1;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      if (f(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
