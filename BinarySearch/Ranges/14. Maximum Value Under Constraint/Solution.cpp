a2c586bab89cc576603bbd29516e4622298526fb
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-18 15:40:13 +0530

    done w/ 14th

--- BinarySearch/Ranges/14. Maximum Value Under Constraint/Solution.cpp
+++ BinarySearch/Ranges/14. Maximum Value Under Constraint/Solution.cpp
@@ -1,12 +1,33 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    long long maxValueUnderConstraint(vector<int>& nums, long long budget) {
-        // Find the maximum integer x such that sum(min(nums[i], x)) <= budget.
-        // Use binary search on x.
-        return 0;
+  long long maxValueUnderConstraint(vector<int> &nums, long long budget) {
+    int n = nums.size();
+    auto check = [&](long long mn) -> bool {
+      long long sum = 0;
+      for (int i = 0; i < n; ++i) {
+        long long add = min((long long)nums[i], mn);
+        if (sum + add > budget)
+          return false;
+        sum += add;
+      }
+      return true;
+    };
+
+    long long low = 1, high = 1e14;
+    long long ans = 0;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        low = mid + 1;
+      } else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
