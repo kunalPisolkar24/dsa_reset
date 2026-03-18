bc09478ee8cb19ced14c8ccf91fa25c88a47a228
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-18 12:12:14 +0530

    done with 10

--- BinarySearch/Ranges/10. Maximum Minimum Distance/Solution.cpp
+++ BinarySearch/Ranges/10. Maximum Minimum Distance/Solution.cpp
@@ -1,11 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
 public:
-    int maxMinDistance(vector<int>& positions, int k) {
-        // Find maximum possible minimum distance between k items placed at given positions (Aggressive Cows).
-        return 0;
+  int maxMinDistance(vector<int> &positions, int k) {
+    int n = positions.size();
+    sort(positions.begin(), positions.end());
+
+    auto check = [&](int dist) -> bool {
+      int placed = 1;
+      int prev = positions[0];
+      for (int i = 1; i < n; i++) {
+        if (positions[i] - prev >= dist) {
+          placed++;
+          prev = positions[i];
+        }
+      }
+      return placed >= k;
+    };
+
+    int low = 0, high = positions[n - 1] - positions[0];
+    int ans = 0;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        low = mid + 1;
+      } else
+        high = mid - 1;
     }
+    return ans;
+  }
 };
