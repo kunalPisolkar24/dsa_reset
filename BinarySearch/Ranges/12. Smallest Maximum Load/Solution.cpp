622195a66e4d49cce0840cdcbc5568de0faafb95
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-18 14:24:02 +0530

    done with 12th

--- BinarySearch/Ranges/12. Smallest Maximum Load/Solution.cpp
+++ BinarySearch/Ranges/12. Smallest Maximum Load/Solution.cpp
@@ -1,14 +1,39 @@
+#include <algorithm>
 #include <bits/stdc++.h>
+#include <numeric>
 
 using namespace std;
 
 class Solution {
 public:
-    long long minMaximumLoad(vector<int>& tasks, int k) {
-        // Implement the "Binary Search on Answer Space" approach here.
-        // Step 1: Define your range [low, high].
-        // Step 2: Implement isPossible(maxLoad) to check if all tasks can be done with load <= mid.
-        // Step 3: Run the binary search to find the minimum of such points.
-        return 0;
+  long long minMaximumLoad(vector<int> &tasks, int k) {
+    int n = tasks.size();
+
+    auto check = [&](int cap) -> bool {
+      int worker = 1;
+      long long sum = 0;
+      for (int i = 0; i < n; i++) {
+        if (sum + tasks[i] > cap) {
+          worker++;
+          sum = 0;
+        }
+        sum += tasks[i];
+      }
+      cout << worker << "---" << cap << endl;
+      return worker <= k;
+    };
+
+    long long low = *max_element(tasks.begin(), tasks.end()),
+              high = accumulate(tasks.begin(), tasks.end(), 0);
+    long long ans = high;
+    while (low <= high) {
+      long long mid = low + (high - low) / 2;
+      if (check(mid)) {
+        ans = mid;
+        high = mid - 1;
+      } else
+        low = mid + 1;
     }
+    return ans;
+  }
 };
