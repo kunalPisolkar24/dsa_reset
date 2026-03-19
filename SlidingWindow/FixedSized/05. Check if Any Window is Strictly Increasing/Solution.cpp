f568bf367cb223d407d5e47017e6b4ba4df5a85d
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 20:57:27 +0530

    done w/ 5th

--- SlidingWindow/FixedSized/05. Check if Any Window is Strictly Increasing/Solution.cpp
+++ SlidingWindow/FixedSized/05. Check if Any Window is Strictly Increasing/Solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    bool isAnyWindowIncreasing(vector<int>& nums, int k) {
-        // Your code goes here
-        return false;
+  bool isAnyWindowIncreasing(vector<int> &nums, int k) {
+    int valid = 0, n = nums.size();
+    for (int i = 0; i < k - 1; i++) {
+      if (nums[i] < nums[i + 1])
+        valid++;
     }
+    if (valid == k - 1)
+      return true;
+    for (int right = k - 1; right < n - 1; right++) {
+      int left = right - k + 1;
+      if (nums[right] < nums[right + 1])
+        valid++;
+      if (nums[left] < nums[left + 1])
+        valid--;
+
+      if (valid == k - 1)
+        return true;
+    }
+    return false;
+  }
 };
