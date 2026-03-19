93506e280ecbb6158322c1ed881211a1e3d57cfc
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 22:17:57 +0530

    done with 1st

--- SlidingWindow/VariableSized/01. Longest Subarray With Sum ≤ K/Solution.cpp
+++ SlidingWindow/VariableSized/01. Longest Subarray With Sum ≤ K/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
+  int longestSubarray(vector<int> &nums, int k) {
+    int left = 0, n = nums.size();
+    int sum = 0, mx = 0;
+    for (int right = 0; right < n; right++) {
+      sum += nums[right];
+      while (sum > k) {
+        sum -= nums[left];
+        left++;
+      }
+      mx = max(mx, right - left + 1);
     }
+    return mx;
+  }
 };
