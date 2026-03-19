f4c8d92b35e8651791e7d4f909cfa6dae8816f3c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 20:33:06 +0530

    done with 4th

--- SlidingWindow/FixedSized/04. Distinct Elements in Every Window/Solution.cpp
+++ SlidingWindow/FixedSized/04. Distinct Elements in Every Window/Solution.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> countDistinct(vector<int>& nums, int k) {
-        // Your code goes here
-        return {};
+  vector<int> countDistinct(vector<int> &nums, int k) {
+    unordered_map<int, int> ump;
+    vector<int> res;
+    int n = nums.size();
+    for (int i = 0; i < k; i++) {
+      ump[nums[i]]++;
     }
+    res.push_back(ump.size());
+    for (int right = k; right < n; right++) {
+      ump[nums[right - k]]--;
+      if (ump[nums[right - k]] == 0) {
+        ump.erase(nums[right - k]);
+      }
+      ump[nums[right]]++;
+      res.push_back(ump.size());
+    }
+    return res;
+  }
 };
