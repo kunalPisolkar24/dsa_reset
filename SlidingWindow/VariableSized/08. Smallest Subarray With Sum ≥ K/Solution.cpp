3710e27214e617351cf3e4c60aef8543397af4bc
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 23:06:44 +0530

    done with 8th

--- SlidingWindow/VariableSized/08. Smallest Subarray With Sum ≥ K/Solution.cpp
+++ SlidingWindow/VariableSized/08. Smallest Subarray With Sum ≥ K/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    int smallestSubarray(vector<int>& nums, int k) {
-        
-        return -1;
+  int smallestSubarray(vector<int> &nums, int k) {
+    int left = 0, n = nums.size(), mn = numeric_limits<int>::max();
+    int sum = 0;
+    for (int right = 0; right < n; right++) {
+      sum += nums[right];
+      while (sum >= k) {
+        mn = min(mn, right - left + 1);
+        sum -= nums[left];
+        left++;
+      }
     }
+    return (mn == numeric_limits<int>::max() ? -1 : mn);
+  }
 };
