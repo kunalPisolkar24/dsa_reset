f5cf136dc91fe25fd9c52362d59b92ea920a01bf
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 19:37:53 +0530

    done w/ 2dn

--- SlidingWindow/FixedSized/02. Maximum Average Subarray Size K/Solution.cpp
+++ SlidingWindow/FixedSized/02. Maximum Average Subarray Size K/Solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    double findMaxAverage(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0.0;
+  double findMaxAverage(vector<int> &nums, int k) {
+    long long sum = 0;
+    int n = nums.size();
+    for (int i = 0; i < k; i++) {
+      sum += nums[i];
     }
+    double mx = sum;
+
+    for (int right = k; right < n; right++) {
+      sum -= nums[right - k];
+      sum += nums[right];
+      mx = max((double) sum, mx);
+    }
+    return mx / k;
+  }
 };
