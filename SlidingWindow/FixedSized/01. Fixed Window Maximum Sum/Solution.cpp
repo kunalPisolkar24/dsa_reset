f63e1203ad9f6490caaf15613a19dec7ad43e267
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 19:27:21 +0530

    done w/ 1st

--- SlidingWindow/FixedSized/01. Fixed Window Maximum Sum/Solution.cpp
+++ SlidingWindow/FixedSized/01. Fixed Window Maximum Sum/Solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int maxWindowSum(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
+  int maxWindowSum(vector<int> &nums, int k) {
+    int sum = 0, n = nums.size(), mx = numeric_limits<int>::min();
+
+    for (int i = 0; i < k; i++)
+      sum += nums[i];
+
+    mx = max(mx, sum);
+
+    for (int right = k; right < n; right++) {
+      sum -= nums[right - k];
+      sum += nums[right];
+      mx = max(mx, sum);
     }
+    return mx;
+  }
 };
