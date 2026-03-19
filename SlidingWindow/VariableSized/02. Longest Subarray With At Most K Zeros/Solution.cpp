4cbb19f7b6cef6e0da867f59627227f0fafcf1a5
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 22:30:48 +0530

    done w/ 7th

--- SlidingWindow/VariableSized/02. Longest Subarray With At Most K Zeros/Solution.cpp
+++ SlidingWindow/VariableSized/02. Longest Subarray With At Most K Zeros/Solution.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
 public:
-    int longestSubarrayWithAtMostKZeros(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
+  int longestSubarrayWithAtMostKZeros(vector<int> &nums, int k) {
+    int left = 0, n = nums.size();
+    int zeroCount = 0, mx = 0;
+    for (int right = 0; right < n; right++) {
+      zeroCount += (nums[right] == 0 ? 1 : 0);
+      while (zeroCount > k) {
+        zeroCount -= (nums[left] == 0 ? 1 : 0);
+        left++;
+      }
+      mx = max(mx, right - left + 1);
     }
+    return mx;
+  }
 };
