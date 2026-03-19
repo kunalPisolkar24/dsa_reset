5a483f9d459d79b2a19e210a8b3fb1420b59d50a
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-19 19:40:47 +0530

    done with 3rd

--- SlidingWindow/FixedSized/03. Maximum Even Numbers in Window/Solution.cpp
+++ SlidingWindow/FixedSized/03. Maximum Even Numbers in Window/Solution.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    int maxEvenNumbers(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
+  int maxEvenNumbers(vector<int> &nums, int k) {
+    int evenCount = 0;
+    int n = nums.size();
+    for (int i = 0; i < k; i++) {
+      if (nums[i] % 2 == 0)
+        evenCount++;
     }
+    
+    int mx = evenCount;
+    for (int right = k; right < n; right++) {
+      evenCount -= (nums[right - k] % 2 == 0 ? 1 : 0);
+      evenCount += (nums[right] % 2 == 0 ? 1 : 0);
+      mx = max(evenCount, mx);
+    }
+
+    return mx;
+  }
 };
