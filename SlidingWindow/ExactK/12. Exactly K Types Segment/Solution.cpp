db8984aba5d68ef6152ee799755af95358a8f1b4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 19:24:10 +0530

    done w/ 12th

--- SlidingWindow/ExactK/12. Exactly K Types Segment/Solution.cpp
+++ SlidingWindow/ExactK/12. Exactly K Types Segment/Solution.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    int countSubstringsWithKDistinct(string s, int k) {
-        // Your code goes here
-        return 0;
-    }
+  int countSubstringsWithKDistinct(string s, int k) {
+    int n = s.size();
+
+    auto sol = [&](int k) -> int {
+      unordered_map<char, int> ump;
+      int left = 0, count = 0;
+      for (int right = 0; right < n; right++) {
+        ump[s[right]]++;
+        while (ump.size() > k) {
+          ump[s[left]]--;
+          if (ump[s[left]] == 0) {
+            ump.erase(s[left]);
+          }
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+
+    return sol(k) - sol(k - 1);
+  }
 };
