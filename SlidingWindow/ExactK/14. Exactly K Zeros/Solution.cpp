2900d2e7ac0a110a24b835e4b7ce03d3abbbcd0a
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 19:49:44 +0530

    done w/ 14th

--- SlidingWindow/ExactK/14. Exactly K Zeros/Solution.cpp
+++ SlidingWindow/ExactK/14. Exactly K Zeros/Solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    int countSubarraysWithKZeros(vector<int>& nums, int k) {
-        // Your code goes here
+  int countSubarraysWithKZeros(vector<int> &nums, int k) {
+    int n = nums.size();
+
+    auto sol = [&](int k) -> int {
+      int left = 0, count = 0, zeroCount = 0;
+      if (k < 0)
         return 0;
-    }
+      for (int right = 0; right < n; right++) {
+        zeroCount += (nums[right] == 0);
+        while (zeroCount > k) {
+          zeroCount -= (nums[left] == 0);
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+
+    return sol(k) - sol(k - 1);
+  }
 };
