607861f10aaf7dd804661914c65208b54c227de4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 12:53:39 +0530

    done w/ 9th

--- SlidingWindow/VariableSized/09. Count of Subarrays With Sum ≤ K/Solution.cpp
+++ SlidingWindow/VariableSized/09. Count of Subarrays With Sum ≤ K/Solution.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
+  long long countSubarrays(vector<int> &nums, int k) {
+    int n = nums.size();
+    if (k == 0)
+      return 0;
+
+    long long count = 0, sum = 0;
+    int left = 0;
+    for (int right = 0; right < n; right++) {
+      sum += nums[right];
+      while (sum > k) {
+        sum -= nums[left];
+        left++;
+      }
+      count += (right - left + 1);
     }
+    return count;
+  }
 };
