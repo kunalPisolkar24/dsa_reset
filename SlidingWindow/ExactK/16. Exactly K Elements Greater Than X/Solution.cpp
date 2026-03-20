8e05f0fc2073b6478f0c5f8d5a3dd915648eeccb
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 20:58:04 +0530

    done w/ 16th

--- SlidingWindow/ExactK/16. Exactly K Elements Greater Than X/Solution.cpp
+++ SlidingWindow/ExactK/16. Exactly K Elements Greater Than X/Solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    int countSubarraysWithKGreater(vector<int>& nums, int k, int x) {
-        // Your code goes here
+  int countSubarraysWithKGreater(vector<int> &nums, int k, int x) {
+    int n = nums.size();
+
+    auto sol = [&](int k) -> int {
+      if (k < 0)
         return 0;
-    }
+      int left = 0, count = 0, greater = 0;
+      for (int right = 0; right < n; right++) {
+        greater += (nums[right] > x);
+        while (greater > k) {
+          greater -= (nums[left] > x);
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+
+    return sol(k) - sol(k - 1);
+  }
 };
