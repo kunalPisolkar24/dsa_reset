4a27a46d000a1a2d9b31bf54ec123b81fda70b02
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 19:55:29 +0530

    done 15th

--- SlidingWindow/ExactK/15. Exactly K Distinct Integers/Solution.cpp
+++ SlidingWindow/ExactK/15. Exactly K Distinct Integers/Solution.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
-    }
+  int subarraysWithKDistinct(vector<int> &nums, int k) {
+    int n = nums.size();
+    
+    auto sol = [&](int k) -> int {
+      unordered_map<int, int> ump;
+      int left = 0, count = 0;
+      for (int right = 0; right < n; right++) {
+        ump[nums[right]]++;
+        while (ump.size() > k) {
+          ump[nums[left]]--;
+          if (ump[nums[left]] == 0) {
+            ump.erase(nums[left]);
+          }
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+
+    return sol(k) - sol(k - 1);
+  }
 };
