5fe349bf1bbbb906c14ec81006a9cba36cf8d94e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 12:29:43 +0530

    done w/ 10th

--- SlidingWindow/VariableSized/10. Longest Substring Without Repeating Characters/Solution.cpp
+++ SlidingWindow/VariableSized/10. Longest Substring Without Repeating Characters/Solution.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        // Your code goes here
-        return 0;
+  int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> ump;
+    int n = s.size();
+    int left = 0, mx = 0;
+
+    for (int right = 0; right < n; right++) {
+      ump[s[right]]++;
+      while (ump[s[right]] > 1) {
+        ump[s[left]]--;
+        if (ump[s[left]] == 0)
+          ump.erase(s[left]);
+        left++;
+      }
+      mx = max(mx, (int)ump.size());
     }
+    return mx;
+  }
 };
