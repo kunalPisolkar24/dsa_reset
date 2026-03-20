21c89d908359055c8d03ed2dc972692106bcb643
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 19:34:34 +0530

    done w/ 13th

--- SlidingWindow/ExactK/13. Balanced Odd Segment/Solution.cpp
+++ SlidingWindow/ExactK/13. Balanced Odd Segment/Solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    int countSubarraysWithKOdds(vector<int>& nums, int k) {
-        // Your code goes here
-        return 0;
-    }
+  int countSubarraysWithKOdds(vector<int> &nums, int k) {
+    int n = nums.size();
+
+    auto sol = [&](int k) -> int {
+      int oddCount = 0;
+      if(k < 0) return 0;
+      int left = 0, count = 0;
+      for (int right = 0; right < n; right++) {
+        oddCount += (nums[right] & 1);
+        while (oddCount > k) {
+          oddCount -= (nums[left] & 1);
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+
+    return sol(k) - sol(k - 1);
+  }
 };
