615d142c394e2b1d44235eedf5ac4ecdca9e9c89
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-20 12:47:46 +0530

    done w/ 11

--- SlidingWindow/VariableSized/11. Longest Substring With At Most K Distinct Characters/Solution.cpp
+++ SlidingWindow/VariableSized/11. Longest Substring With At Most K Distinct Characters/Solution.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstringKDistinct(string s, int k) {
-        // Your code goes here
-        return 0;
+  int lengthOfLongestSubstringKDistinct(string s, int k) {
+    int left = 0, n = s.size(), mx = 0;
+    unordered_map<char, int> ump;
+    if(k == 0) return 0;
+    for (int right = 0; right < n; right++) {
+      ump[s[right]]++;
+      while (ump.size() > k) {
+        ump[s[left]]--;
+        if (ump[s[left]] == 0)
+          ump.erase(s[left]);
+        left++;
+      }
+      mx = max(mx, right - left + 1);
     }
+    return mx;
+  }
 };
