94684580b726e09ad510054b00a3a91148e8188c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:32:24 +0530

    done w/ 8th

--- Recursion/08. Count Zeros in a Number/Solution.cpp
+++ Recursion/08. Count Zeros in a Number/Solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    int countZeros(int n) {
-        // Your code goes here
+  int countZeros(int N) {
+    if(N == 0) return 1;
+    function<int(int)> sol = [&](int n) -> int {
+      if (n <= 0)
         return 0;
-    }
+      int temp = n % 10;
+      return (temp == 0) + sol(n / 10);
+    };
+    if (N < 0)
+      N *= -1;
+    return sol(N);
+  }
 };
