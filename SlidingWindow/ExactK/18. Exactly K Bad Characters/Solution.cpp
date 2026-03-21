c3fb3f9d8c6bb2a9f1b9dfcbd6c3d3d2f59bedd0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 12:27:56 +0530

    done 18th

--- SlidingWindow/ExactK/18. Exactly K Bad Characters/Solution.cpp
+++ SlidingWindow/ExactK/18. Exactly K Bad Characters/Solution.cpp
@@ -4,8 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    int countSubstringsWithKBad(string s, vector<char>& bad, int k) {
-        // Your code goes here
-        return 0;
+  int countSubstringsWithKBad(string s, vector<char> &bad, int k) {
+    int n = s.size();
+    unordered_set<char> bads;
+    for (int i = 0; i < bad.size(); i++) {
+      bads.insert(bad[i]);
     }
+
+    auto sol = [&](int k) -> int {
+      if (k < 0)
+        return 0;
+      int left = 0, count = 0, badCount = 0;
+      for (int right = 0; right < n; right++) {
+        badCount += (bads.count(s[right]));
+        while (badCount > k) {
+          badCount -= (bads.count(s[left]));
+          left++;
+        }
+        count += (right - left + 1);
+      }
+      return count;
+    };
+    return sol(k) - sol(k - 1);
+  }
 };
