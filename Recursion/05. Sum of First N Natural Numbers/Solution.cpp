9e5b4a5fa99950e0f375eec0774f24d9ff4a6b3f
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:18:07 +0530

    done w/ 5th

--- Recursion/05. Sum of First N Natural Numbers/Solution.cpp
+++ Recursion/05. Sum of First N Natural Numbers/Solution.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 class Solution {
 public:
-    int sumFirstN(int n) {
-        // Your code goes here
+  int sumFirstN(int N) {
+    function<int(int)> sol = [&](int n) -> int {
+      if (n < 0)
         return 0;
-    }
+      return n + sol(n - 1);
+    };
+    return sol(N);
+  }
 };
