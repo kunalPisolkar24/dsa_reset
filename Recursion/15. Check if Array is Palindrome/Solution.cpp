3250be83fa5c9ae922130c3054274d4a1b0790db
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 17:03:59 +0530

    done w/ 15th

--- Recursion/15. Check if Array is Palindrome/Solution.cpp
+++ Recursion/15. Check if Array is Palindrome/Solution.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(vector<int>& arr) {
-        // Your code goes here
-        return true;
-    }
+  bool isPalindrome(vector<int> &arr) {
+    bool res = true;
+    function<void(int, int)> sol = [&](int low, int high) -> void {
+      if(!res) return;
+      if (low <= high) {
+        if (arr[low] != arr[high]) {
+          res = false;
+          return;
+        }
+        sol(low + 1, high - 1);
+      }
+      return;
+    };
+    int n = arr.size();
+    sol(0, n - 1);
+    return res;
+  }
 };
