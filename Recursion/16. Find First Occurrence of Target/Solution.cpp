01b447c249ab686f678a66a4bc8fa8ded00ac0b3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 17:14:35 +0530

    done w/ 16th

--- Recursion/16. Find First Occurrence of Target/Solution.cpp
+++ Recursion/16. Find First Occurrence of Target/Solution.cpp
@@ -5,7 +5,17 @@ using namespace std;
 class Solution {
 public:
     int findFirst(vector<int>& arr, int target) {
-        // Your code goes here
-        return -1;
+        int n = arr.size(); 
+        int ans = -1;
+        function<void(int)> sol = [&](int idx) -> void {
+            if(ans != -1 or idx >= n) return;
+            if(arr[idx] == target) {
+                ans = idx;
+                return;
+            }
+            sol(idx + 1);
+        };
+        sol(0);
+        return ans;
     }
 };
