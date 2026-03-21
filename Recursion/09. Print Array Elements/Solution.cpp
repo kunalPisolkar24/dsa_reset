3e04e887abf0aa2b5fd3fb3c2ecedc21849685cf
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:38:14 +0530

    done w/ 9th

--- Recursion/09. Print Array Elements/Solution.cpp
+++ Recursion/09. Print Array Elements/Solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> printArray(vector<int>& arr) {
-        // Your code goes here
-        return {};
-    }
+  vector<int> printArray(vector<int> &arr) {
+    vector<int> res;
+    int n = arr.size();
+    function<void(int)> sol = [&](int idx) -> void {
+      if (idx == n)
+        return;
+      res.push_back(arr[idx]);
+      sol(idx + 1);
+    };
+    sol(0);
+    return res;
+  }
 };
