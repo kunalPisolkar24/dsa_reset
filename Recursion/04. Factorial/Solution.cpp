588f089b82c07f1c357dc4c8c45fcb518ccfe1c2
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:15:32 +0530

    done w/ 4th

--- Recursion/04. Factorial/Solution.cpp
+++ Recursion/04. Factorial/Solution.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 class Solution {
 public:
-    long long factorial(int n) {
-        // Your code goes here
-        return 0;
-    }
+  long long factorial(int N) {
+    function<long long(int)> sol = [&](int n) -> long long {
+      if (n == 0 or n == 1)
+        return 1;
+      return n * sol(n - 1);
+    };
+
+    return sol(N);
+  }
 };
