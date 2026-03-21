783a29e66c63772d4d29dc3bd4875ea3dd457e95
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:24:38 +0530

    done w/ 6th

--- Recursion/06. Sum of Digits of a Number/Solution.cpp
+++ Recursion/06. Sum of Digits of a Number/Solution.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    int sumOfDigits(int n) {
-        // Your code goes here
+  int sumOfDigits(int N) {
+    function<int(int)> sol = [&](int n) -> int {
+      if (n <= 0)
         return 0;
-    }
+      int tempVal = n % 10;
+      return tempVal + sol(n / 10);
+    };
+    if (N < 0)
+      N *= -1;
+    return sol(N);
+  }
 };
