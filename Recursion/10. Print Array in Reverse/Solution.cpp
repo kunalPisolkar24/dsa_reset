59f161dada8b2efd75eb041fc924f0375f144512
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:40:30 +0530

    done w/ 10th

--- Recursion/10. Print Array in Reverse/Solution.cpp
+++ Recursion/10. Print Array in Reverse/Solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> printReverse(vector<int>& arr) {
-        // Your code goes here
-        return {};
-    }
+  vector<int> printReverse(vector<int> &arr) {
+    vector<int> res;
+    int n = arr.size();
+    function<void(int)> sol = [&](int idx) -> void {
+      if (idx < 0)
+        return;
+      res.push_back(arr[idx]);
+      sol(idx - 1);
+    };
+    sol(n - 1);
+    return res;
+  }
 };
