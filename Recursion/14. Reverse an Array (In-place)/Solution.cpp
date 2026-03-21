abd5512fb0405095ba11e756394c55c0ccc01d99
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 17:01:46 +0530

    done w/ 14th

--- Recursion/14. Reverse an Array (In-place)/Solution.cpp
+++ Recursion/14. Reverse an Array (In-place)/Solution.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    void reverseArray(vector<int>& arr) {
-        // Your code goes here
-    }
+  void reverseArray(vector<int> &arr) {
+    function<void(int, int)> sol = [&](int low, int high) -> void {
+      if (low <= high) {
+        swap(arr[low], arr[high]);
+        sol(low + 1, high - 1);
+      }
+      return;
+    };
+    int n = arr.size();
+    sol(0, n - 1);
+  }
 };
