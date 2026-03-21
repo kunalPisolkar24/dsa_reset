5c2ea58dfc8783ea44dd47ffcfb4e768b24b6e58
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:10:04 +0530

    done w/ 1st

--- Recursion/01. Print 1 to N/Solution.cpp
+++ Recursion/01. Print 1 to N/Solution.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> print1ToN(int n) {
-        // Your code goes here
-        return {};
-    }
+  vector<int> print1ToN(int N) {
+    vector<int> res;
+    function<void(int)> sol = [&](int n) -> void {
+      if (n > N)
+        return;
+      res.push_back(n);
+      sol(n + 1);
+    };
+    sol(1);
+    return res;
+  }
 };
