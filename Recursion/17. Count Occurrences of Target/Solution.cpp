3b1b09b04c495a9915bb5647f636ec694566b851
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 17:17:34 +0530

    done w/ 17th

--- Recursion/17. Count Occurrences of Target/Solution.cpp
+++ Recursion/17. Count Occurrences of Target/Solution.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    int countOccurrences(vector<int>& arr, int target) {
-        // Your code goes here
-        return 0;
-    }
+  int countOccurrences(vector<int> &arr, int target) {
+    int n = arr.size();
+    int ans = 0;
+    function<void(int)> sol = [&](int idx) -> void {
+      if (idx >= n)
+        return;
+    
+      if (arr[idx] == target) {
+        ans++;
+      }
+
+      sol(idx + 1);
+    };
+    sol(0);
+    return ans;
+  }
 };
