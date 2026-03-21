5e9496eaf90f891939a15f44580ad75a612c9c6f
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:29:01 +0530

    done w/ 7th

--- Recursion/07. Count Digits in a Number/Solution.cpp
+++ Recursion/07. Count Digits in a Number/Solution.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 class Solution {
 public:
-    int countDigits(int n) {
-        // Your code goes here
+  int countDigits(int N) {
+    if(N == 0) return 1;
+    function<int(int)> sol = [&](int n) -> int {
+      if (n <= 0)
         return 0;
-    }
+      return 1 + sol(n / 10);
+    };
+    if(N < 0) N *= -1;
+    return sol(N);
+  }
 };
