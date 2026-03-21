070f5f88f33865f4c01114808ba98cbae78318a4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-21 16:55:08 +0530

    done w/ 13

--- Recursion/13. Check if Array is Sorted/Solution.cpp
+++ Recursion/13. Check if Array is Sorted/Solution.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool isSorted(vector<int>& arr) {
-        // Your code goes here
-        return true;
-    }
+  bool isSorted(vector<int> &arr) {
+    int n = arr.size();
+    if (n == 1)
+      return true;
+    bool res = true;
+    function<void(int)> sol = [&](int idx) -> void {
+      if (!res or idx >= n)
+        return;
+      if (arr[idx - 1] > arr[idx]) {
+        res = false;
+      }
+      sol(idx + 1);
+    };
+    sol(1);
+    return res;
+  }
 };
