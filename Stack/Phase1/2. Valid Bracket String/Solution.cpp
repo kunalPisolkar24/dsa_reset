84f592101af8a836c24f1db34e260f23ae600fd5
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-22 14:51:39 +0530

    done w/ 2nd

--- Stack/Phase1/2. Valid Bracket String/Solution.cpp
+++ Stack/Phase1/2. Valid Bracket String/Solution.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    bool isValid(string s) {
-        // Your code here
-        return false;
+  bool isValid(string s) {
+    string opening = "{[(";
+    stack<char> st;
+    for (auto i : s) {
+      if (opening.find(i) != string::npos) {
+        st.push(i);
+      } else {
+        if (!st.empty()) {
+          if (st.top() == '(' and i == ')')
+            st.pop();
+          else if (st.top() == '[' and i == ']')
+            st.pop();
+          else if (st.top() == '{' and i == '}')
+            st.pop();
+          else
+            return false;
+        } else
+          return false;
+      }
     }
+    return st.empty();
+  }
 };
