8c212e0a77e95797d45b5f7de9d7311fff99c817
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-22 19:23:45 +0530

    done w/ 7th

--- Stack/Phase1/7. Remove K Adjacent Duplicates/Solution.cpp
+++ Stack/Phase1/7. Remove K Adjacent Duplicates/Solution.cpp
@@ -4,8 +4,35 @@ using namespace std;
 
 class Solution {
 public:
-    string removeDuplicates(string s, int k) {
-        // Your code here
-        return "";
+  string removeDuplicates(string s, int k) {
+    stack<pair<char, int>> st;
+
+    for (auto i : s) {
+      if (st.empty()) {
+        st.push({i, 1});
+        continue;
+      }
+
+      if (st.top().first == i) {
+        st.push({i, st.top().second + 1});
+        if (st.top().second >= k) {
+          int temp = k;
+          while (!st.empty() and temp--) {
+            st.pop();
+          }
+        }
+      } else {
+        st.push({i, 1});
+      }
     }
+
+    string res;
+    while (!st.empty()) {
+      res += st.top().first;
+      st.pop();
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+  }
 };
