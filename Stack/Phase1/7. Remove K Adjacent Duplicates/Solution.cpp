193eb65e7dc740c966e0902e8d6590f9baddc3e8
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-22 15:14:41 +0530

    add 7th

---
+++ Stack/Phase1/7. Remove K Adjacent Duplicates/Solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        // Your code here
+        return "";
+    }
+};
