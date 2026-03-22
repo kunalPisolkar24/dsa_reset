16034eae2562b1479da2dbc5d9d3a7a830536a7a
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-22 15:05:36 +0530

    done w/ 3rd

--- Stack/Phase1/3. Remove Adjacent Duplicates/Solution.cpp
+++ Stack/Phase1/3. Remove Adjacent Duplicates/Solution.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    string removeDuplicates(string s) {
-        // Your code here
-        return "";
+  string removeDuplicates(string s) {
+    string res = "";
+    for (auto i : s) {
+      if (res.empty()) {
+        res.push_back(i);
+        continue;
+      }
+      if (!res.empty() and i == res.back()) {
+        res.pop_back();
+      } else
+        res.push_back(i);
     }
+    return res;
+  }
 };
