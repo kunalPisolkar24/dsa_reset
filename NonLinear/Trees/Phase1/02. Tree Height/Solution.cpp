46bcb94d84f6c6942d75934e15e764ea17c3d197
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 16:14:19 +0530

    done w/ 2dn

--- NonLinear/Trees/Phase1/02. Tree Height/Solution.cpp
+++ NonLinear/Trees/Phase1/02. Tree Height/Solution.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        return {};
-    }
+  int maxDepth(TreeNode *root) {
+    if (!root)
+      return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+  }
 };
