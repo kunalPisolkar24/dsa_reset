03a5bc0ca0d8da8c69645336879032a2d6192f3c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 16:17:28 +0530

    done w/ 3rd

--- NonLinear/Trees/Phase1/03. Count Leaf Nodes/Solution.cpp
+++ NonLinear/Trees/Phase1/03. Count Leaf Nodes/Solution.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int countLeaves(TreeNode* root) {
-        return {};
-    }
+  int countLeaves(TreeNode *root) {
+    if (!root)
+      return 0;
+    if (!root->left and !root->right)
+      return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+  }
 };
