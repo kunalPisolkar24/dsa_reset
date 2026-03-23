bf49b6364dea776d6f8dc66e29a6963cddd2b4f3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 11:30:06 +0530

    done w/ 10th

--- Stack/Phase1/10. Build Array With Stack Operations/Solution.cpp
+++ Stack/Phase1/10. Build Array With Stack Operations/Solution.cpp
@@ -4,8 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> buildArray(vector<int>& target, int n) {
-        // Your code here
-        return {};
+  vector<string> buildArray(vector<int> &target, int n) {
+    stack<int> st;
+    int completed = 0;
+    vector<string> res;
+    for (int i = 1; i <= n; i++) {
+      if (completed == target.size()) {
+        break;
+      }
+
+      st.push(i);
+      res.push_back("Push");
+
+      if (i == target[completed]) {
+        completed++;
+      } else {
+        st.pop();
+        res.push_back("Pop");
+      }
     }
+    return res;
+  }
 };
