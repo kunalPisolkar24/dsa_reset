d82d44f1afe7ce023a494805a7abce89d45d45e7
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 10:32:35 +0530

    done w/ 6th

--- Stack/Phase1/6. Simplify Path/Solution.cpp
+++ Stack/Phase1/6. Simplify Path/Solution.cpp
@@ -4,8 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    string simplifyPath(string path) {
-        // Your code here
-        return "";
+  string simplifyPath(string path) {
+    stack<string> st;
+    string token;
+    stringstream ss(path);
+
+    while (getline(ss, token, '/')) {
+      if (token.empty() or token == ".")
+        continue;
+      if (token == "..") {
+        if (!st.empty())
+          st.pop();
+      } else {
+        cout << token << endl;
+        st.push(token);
+      }
     }
+
+    vector<string> tokens;
+    while (!st.empty()) {
+      tokens.push_back(st.top());
+      st.pop();
+    }
+    reverse(tokens.begin(), tokens.end());
+
+    string res;
+    for (auto i : tokens) {
+      res += "/" + i;
+    }
+    return res.empty() ? "/" : res;
+  }
 };
