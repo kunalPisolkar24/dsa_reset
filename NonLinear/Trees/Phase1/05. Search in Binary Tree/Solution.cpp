d92f4f55faaba4a4e140897f028e8e9aaf103bdd
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 16:34:20 +0530

    done w/ 5th

--- NonLinear/Trees/Phase1/05. Search in Binary Tree/Solution.cpp
+++ NonLinear/Trees/Phase1/05. Search in Binary Tree/Solution.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    bool searchNode(TreeNode* root, int val) {
-        return {};
-    }
+  bool searchNode(TreeNode *root, int val) {
+    if (!root)
+      return false;
+    if (root->val == val)
+      return true;
+    return searchNode(root->left, val) || searchNode(root->right, val);
+  }
 };
