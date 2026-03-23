40b6e2bfc916c5c1503be9d7b0ccb3a05f63111e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 16:18:26 +0530

    done w/ 4th

--- NonLinear/Trees/Phase1/04. Sum of All Nodes/Solution.cpp
+++ NonLinear/Trees/Phase1/04. Sum of All Nodes/Solution.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int sumOfNodes(TreeNode* root) {
-        return {};
-    }
+  int sumOfNodes(TreeNode *root) {
+    if (!root)
+      return 0;
+    return root->val + sumOfNodes(root->left) + sumOfNodes(root->right);
+  }
 };
