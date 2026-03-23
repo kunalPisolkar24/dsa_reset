a05a9aa54cb635d2655578685a2911f1f300d4e0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 11:18:11 +0530

    done w/ 9th

--- Stack/Phase1/9. Make String Great/Solution.cpp
+++ Stack/Phase1/9. Make String Great/Solution.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
+#include <cctype>
 
 using namespace std;
 
 class Solution {
 public:
-    string makeGood(string s) {
-        // Your code here
-        return "";
+  string makeGood(string s) {
+    string temp;
+
+    auto isLower = [&](char c) -> bool { return (c >= 'a' and c <= 'z'); };
+
+    auto isUpper = [&](char c) -> bool { return (c >= 'A' and c <= 'Z'); };
+
+    for (auto i : s) {
+      if (temp.empty()) {
+        temp.push_back(i);
+      } else {
+        if (isLower(temp.back()) and isUpper(i) and temp.back() == i + 32)
+          temp.pop_back();
+        else if (isUpper(temp.back()) and isLower(i) and temp.back() == i - 32) {
+          temp.pop_back();
+        } else
+          temp.push_back(i);
+      }
     }
+    return temp;
+  }
 };
