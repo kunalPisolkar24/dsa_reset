ceceb8ebb230d24c271ec33c9ccc5cd65673e081
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-23 10:45:29 +0530

    done w/ 8th

--- Stack/Phase1/8. Backspace String Compare/Solution.cpp
+++ Stack/Phase1/8. Backspace String Compare/Solution.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
-        // Your code here
-        return false;
+  bool backspaceCompare(string s, string t) {
+    string a, b;
+    for (auto i : s) {
+      if (!a.empty() and i == '#') {
+        a.pop_back();
+      } else
+        if(i != '#') 
+            a.push_back(i);
     }
+
+    for(auto i: t) {
+      if (!b.empty() and i == '#') {
+        b.pop_back();
+      } else
+        if(i != '#') 
+            b.push_back(i);
+    }
+    return a == b;
+  }
 };
