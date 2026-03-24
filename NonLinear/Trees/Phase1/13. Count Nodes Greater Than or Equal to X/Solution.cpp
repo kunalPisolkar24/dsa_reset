dc40a5f4aeff46d920cdaee20f281270fb449806
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 13:12:21 +0530

    done w/ 13th

--- NonLinear/Trees/Phase1/13. Count Nodes Greater Than or Equal to X/Solution.cpp
+++ NonLinear/Trees/Phase1/13. Count Nodes Greater Than or Equal to X/Solution.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int countNodesGE(TreeNode* root, int x) {
-        return {};
-    }
+  int countNodesGE(TreeNode *root, int x) {
+    int count = 0;
+    function<void(TreeNode *)> sol = [&](TreeNode *root) -> void {
+      if (!root)
+        return;
+      if(root->val >= x) count++;
+      sol(root->left);
+      sol(root->right);
+    };
+    sol(root);
+    return count;
+  }
 };
