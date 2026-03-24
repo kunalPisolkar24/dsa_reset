6b82bf09a221ff84e98b921a3a9bbd1d66c854b6
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 10:59:54 +0530

    done w/ 9th

--- NonLinear/Trees/Phase1/09. Level Order Traversal/Solution.cpp
+++ NonLinear/Trees/Phase1/09. Level Order Traversal/Solution.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        return {};
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    vector<vector<int>> res;
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+      int lev = q.size();
+      vector<int> temp;
+      for (int i = 0; i < lev; i++) {
+        TreeNode *node = q.front();
+        q.pop();
+        temp.push_back(node->val);
+
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+      res.push_back(temp);
     }
+    return res;
+  }
 };
