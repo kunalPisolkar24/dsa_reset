e94e8b61f13bf967d35643e1fa0b095cf26a8d9e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 13:47:07 +0530

    done w/ 15th

--- NonLinear/Trees/Phase1/15. Check for Node with Child Sum Property/Solution.cpp
+++ NonLinear/Trees/Phase1/15. Check for Node with Child Sum Property/Solution.cpp
@@ -3,17 +3,32 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    bool hasChildSum(TreeNode* root) {
-        return {};
-    }
+  bool hasChildSum(TreeNode *root) {
+    bool res = false;
+
+    function<bool(TreeNode *)> sol = [&](TreeNode *node) -> bool {
+      if (!node)
+        return 0;
+      int left = node->left ? node->left->val : 0;
+      int right = node->right ? node->right->val : 0;
+      if ((left + right) == node->val) {
+        return true;
+      }
+
+      return sol(node->left) or sol(node->right);
+    };
+
+    return sol(root);
+  }
 };
