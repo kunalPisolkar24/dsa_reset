3f0def8ac8d3b4184eff792a7be0cb045eeff574
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 11:18:52 +0530

    done w/ 10th

--- NonLinear/Trees/Phase1/10. Maximum Value in Tree/Solution.cpp
+++ NonLinear/Trees/Phase1/10. Maximum Value in Tree/Solution.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int findMax(TreeNode* root) {
-        return {};
-    }
+  int findMax(TreeNode *root) {
+    int mx = numeric_limits<int>::min();
+    function<void(TreeNode *)> sol = [&](TreeNode *root) -> void {
+      if (!root)
+        return;
+      mx = max(root->val, mx);
+      sol(root->left);
+      sol(root->right);
+    };
+    sol(root);
+    return mx;
+  }
 };
