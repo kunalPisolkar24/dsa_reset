18203f78fb098ff99835ec542d05580e3259c859
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 10:53:49 +0530

    done w/ 7th

--- NonLinear/Trees/Phase1/07. Inorder Traversal/Solution.cpp
+++ NonLinear/Trees/Phase1/07. Inorder Traversal/Solution.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        return {};
-    }
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    function<void(TreeNode *)> sol = [&](TreeNode *root) -> void {
+      if (!root)
+        return;
+      sol(root->left);
+      res.push_back(root->val);
+      sol(root->right);
+    };
+    sol(root);
+    return res;
+  }
 };
