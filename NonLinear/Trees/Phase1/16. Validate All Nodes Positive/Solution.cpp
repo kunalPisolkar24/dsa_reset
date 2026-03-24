0b1ad2a54820ace1dfdd2a4a96e3fc38fa576d78
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-24 13:50:33 +0530

    done w/ 16th

--- NonLinear/Trees/Phase1/16. Validate All Nodes Positive/Solution.cpp
+++ NonLinear/Trees/Phase1/16. Validate All Nodes Positive/Solution.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    bool allPositive(TreeNode* root) {
-        return {};
-    }
+  bool allPositive(TreeNode *root) {
+    if (!root)
+      return true;
+    if (root->val < 0)
+      return false;
+    return allPositive(root->left) or allPositive(root->right);
+  }
 };
