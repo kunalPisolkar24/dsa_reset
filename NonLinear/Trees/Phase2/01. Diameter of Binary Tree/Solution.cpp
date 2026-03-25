786f10f664122bd2bee8ef0f3caebd51966ea160
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-25 12:13:15 +0530

    done w/ 1st

--- NonLinear/Trees/Phase2/01. Diameter of Binary Tree/Solution.cpp
+++ NonLinear/Trees/Phase2/01. Diameter of Binary Tree/Solution.cpp
@@ -15,19 +15,14 @@ struct TreeNode {
 class Solution {
 public:
   int diameterOfBinaryTree(TreeNode *root) {
-    function<int(TreeNode *)> height = [&](TreeNode *node) -> int {
+    int mx = 0;
+    function<int(TreeNode *)> sol = [&](TreeNode *node) -> int {
       if (!node)
         return 0;
-      return 1 + max(height(node->left), height(node->right));
-    };
-
-    int mx = numeric_limits<int>::min();
-    function<void(TreeNode *)> sol = [&](TreeNode *node) -> void {
-      if (!node)
-        return;
-      mx = max(mx, height(node->left) + height(node->right));
-      sol(node->left);
-      sol(node->right);
+      int left = sol(node->left);
+      int right = sol(node->right);
+      mx = max(mx, left + right);
+      return 1 + max(left, right);
     };
 
     sol(root);
