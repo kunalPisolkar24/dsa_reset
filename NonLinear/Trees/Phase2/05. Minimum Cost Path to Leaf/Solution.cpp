f0bdcc6a56be787feb6a38a79dd92ccc8f33ed65
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-26 12:34:12 +0530

    done w/ 5th

--- NonLinear/Trees/Phase2/05. Minimum Cost Path to Leaf/Solution.cpp
+++ NonLinear/Trees/Phase2/05. Minimum Cost Path to Leaf/Solution.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int minCostPath(TreeNode* root) {
-        return {};
+  int minCostPath(TreeNode *root) {
+    if (!root)
+      return 0;
+    if (!root->left and !root->right) {
+      return root->val;
     }
+
+    if (!root->left) {
+      return root->val + minCostPath(root->right);
+    }
+    if (!root->right) {
+      return root->val + minCostPath(root->left);
+    }
+
+    return root->val + min(minCostPath(root->left), minCostPath(root->right));
+  }
 };
