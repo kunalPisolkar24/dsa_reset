a255f4342b96ac31e62e7affbdf7dcf161d55c82
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-26 11:42:58 +0530

    done w/ 3rd

--- NonLinear/Trees/Phase2/03. Minimum Depth of Binary Tree/Solution.cpp
+++ NonLinear/Trees/Phase2/03. Minimum Depth of Binary Tree/Solution.cpp
@@ -3,17 +3,28 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int minDepth(TreeNode* root) {
-        return {};
-    }
+  int minDepth(TreeNode *root) {
+    if(!root) return 0;
+    if (!root->left and !root->right)
+      return 1;
+
+    if (!root->left)
+      return 1 + minDepth(root->right);
+
+    if (!root->right)
+      return 1 + minDepth(root->left);
+
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+  }
 };
