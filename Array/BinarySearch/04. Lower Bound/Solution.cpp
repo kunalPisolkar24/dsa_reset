e069967fd8ac3278f39e0a580fe6c75931d238e6
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-29 18:44:40 +0530

    done with 4th

--- Array/BinarySearch/04. Lower Bound/Solution.cpp
+++ Array/BinarySearch/04. Lower Bound/Solution.cpp
@@ -5,7 +5,12 @@ using namespace std;
 class Solution {
 public:
   int lowerBound(vector<int> &nums, int target) {
-    // Your code here
-    return -1;
+    int low = 0, high = nums.size();
+    while(low < high) {
+      int mid = low + (high - low) / 2;
+      if(nums[mid] >= target) high = mid;
+      else low = mid + 1;
+    }
+    return low;
   }
 };
