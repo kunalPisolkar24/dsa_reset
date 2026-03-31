b0ac42af9a00fb69e3eb43775d1516348d24f1d3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-31 14:05:56 +0530

    done w/ 19th

--- Recursion/19. Remove All Occurrences of X from Array/Solution.cpp
+++ Recursion/19. Remove All Occurrences of X from Array/Solution.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> removeX(vector<int>& arr, int x) {
-        // Your code goes here
-        return {};
-    }
+  vector<int> removeX(vector<int> &arr, int x) {
+    function<void(vector<int>&)> sol = [&](vector<int> &a) -> void {
+        if(a.empty()) return;
+        int temp = a.back();
+        a.pop_back();
+        sol(a);
+        if(temp != x) a.push_back(temp);
+    };
+    sol(arr);
+    return arr;
+  }
 };
