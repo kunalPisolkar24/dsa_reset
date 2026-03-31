decaf63176fc52f7e7f507988f0180119471b62b
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-31 13:47:50 +0530

    done w/ 18th

--- Recursion/18. Find Last Occurrence of Target/Solution.cpp
+++ Recursion/18. Find Last Occurrence of Target/Solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    int findLast(vector<int>& arr, int target) {
-        // Your code goes here
+  int findLast(vector<int> &arr, int target) {
+    function<int(int)> sol = [&](int idx) -> int {
+      if (idx < 0)
         return -1;
-    }
+      if (arr[idx] == target) {
+        return idx;
+      }
+      return sol(idx - 1);
+    };
+
+    return sol(arr.size() - 1);
+  }
 };
