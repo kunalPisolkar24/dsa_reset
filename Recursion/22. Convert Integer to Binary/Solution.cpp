a9454c1ebf961e03865216aab1ef60d0d0bc151e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-31 15:33:22 +0530

    done w/ 22

--- Recursion/22. Convert Integer to Binary/Solution.cpp
+++ Recursion/22. Convert Integer to Binary/Solution.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 class Solution {
 public:
-    string convertToBinary(int n) {
-        // Your code goes here
-        return "";
-    }
+  string convertToBinary(int n) {
+    function<string(int)> sol = [&](int n) -> string {
+      if (n == 1)
+        return "1";
+      if (n == 0)
+        return "0";
+      return sol(n / 2) + to_string(n & 1);
+    };
+    return sol(n);
+  }
 };
