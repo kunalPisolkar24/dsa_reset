5edb4061536cddb2c5ec34e9c48b8e2bee70fdb4
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-31 14:37:39 +0530

    done w/ 21

--- Recursion/21. Reverse a String/Solution.cpp
+++ Recursion/21. Reverse a String/Solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    string reverseString(string s) {
-        // Your code goes here
-        return "";
-    }
+  string reverseString(string s) {
+    string temp = s;
+
+    function<void(int, int)> sol = [&](int low, int high) -> void {
+      if (low > high)
+        return;
+      swap(temp[low], temp[high]);
+      sol(low + 1, high - 1);
+    };
+    sol(0, temp.size() - 1);
+    return temp;
+  }
 };
