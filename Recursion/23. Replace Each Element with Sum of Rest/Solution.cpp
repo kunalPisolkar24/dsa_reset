36d6eef73d8101bbd37a7f381f673537018afaae
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-03-31 15:46:00 +0530

    done w/ 23rd

--- Recursion/23. Replace Each Element with Sum of Rest/Solution.cpp
+++ Recursion/23. Replace Each Element with Sum of Rest/Solution.cpp
@@ -5,22 +5,14 @@ using namespace std;
 class Solution {
 public:
   vector<int> replaceWithSum(vector<int> &arr) {
-    function<int(int)> getSum = [&](int idx) -> int {
-      if (idx >= arr.size())
-        return 0;
-      return getSum(idx + 1) + arr[idx];
-    };
-
-    int sum = getSum(0);
-    vector<int> res;
+    int sum = 0;
     function<void(int)> sol = [&](int idx) -> void {
-      if (idx >= arr.size())
-        return;
-      res.push_back(sum - arr[idx]);
-      sol(idx + 1);
+        if(idx >= arr.size()) return;
+        sum += arr[idx];
+        sol(idx + 1);
+        arr[idx] = sum - arr[idx];
     };
-
     sol(0);
-    return res;
+    return arr;
   }
 };
