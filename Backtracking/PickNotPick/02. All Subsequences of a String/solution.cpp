6079c986fd6790057f702bf10f564bc16d56ad84
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-02 09:04:45 +0530

    done w/ 2nd

--- Backtracking/PickNotPick/02. All Subsequences of a String/solution.cpp
+++ Backtracking/PickNotPick/02. All Subsequences of a String/solution.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> subsequences(string s) {
-        // Implement your solution here
-        return {};
-    }
+  vector<string> subsequences(string s) {
+    vector<string> res;
+    int n = s.size();
+    function<void(string &, int)> sol = [&](string &temp, int idx) -> void {
+      if (idx >= n) {
+        res.push_back(temp);
+        return;
+      }
+
+      sol(temp, idx + 1);
+
+      temp.push_back(s[idx]);
+      sol(temp, idx + 1);
+      temp.pop_back();
+    };
+    string temp;
+    sol(temp, 0);
+    return res;
+  }
 };
