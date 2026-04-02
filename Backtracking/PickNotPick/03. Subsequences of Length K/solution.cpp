9c7664588f535a306218b1b50f181f64113da9c6
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-02 09:13:31 +0530

    done w/ 3rd

--- Backtracking/PickNotPick/03. Subsequences of Length K/solution.cpp
+++ Backtracking/PickNotPick/03. Subsequences of Length K/solution.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> subsequencesOfLengthK(vector<int>& arr, int k) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> subsequencesOfLengthK(vector<int> &arr, int k) {
+    int n = arr.size();
+    vector<vector<int>> res;
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      if (temp.size() > k)
+        return;
+
+      if (idx >= n) {
+        if (temp.size() == k) {
+          res.push_back(temp);
+        }
+        return;
+      }
+
+      sol(temp, idx + 1);
+
+      temp.push_back(arr[idx]);
+      sol(temp, idx + 1);
+      temp.pop_back();
+    };
+    vector<int> temp;
+    sol(temp, 0);
+    return res;
+  }
 };
