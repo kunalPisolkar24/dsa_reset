69e1b3afc7614852b7929bf07a369dbf57d2879d
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-02 08:58:28 +0530

    done w/ 1st

--- Backtracking/PickNotPick/01. Binary Strings of Length N/solution.cpp
+++ Backtracking/PickNotPick/01. Binary Strings of Length N/solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> generate(int n) {
-        // Implement your solution here
-        return {};
-    }
+  vector<string> generate(int n) {
+    vector<string> res;
+    function<void(string&)> sol = [&](string &temp) -> void {
+      if (temp.size() == n) {
+        res.push_back(temp);
+        return;
+      }
+
+      temp.push_back('0');
+      sol(temp);
+      temp.pop_back();
+
+      temp.push_back('1');
+      sol(temp);
+      temp.pop_back();
+    };
+    string temp;
+    sol(temp);
+    return res;
+  }
 };
