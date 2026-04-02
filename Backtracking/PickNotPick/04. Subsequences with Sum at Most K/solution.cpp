77517f3fcdb1100ece009d4631d2d0f6b9f2c463
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-02 09:21:14 +0530

    done w/ 4th problem

--- Backtracking/PickNotPick/04. Subsequences with Sum at Most K/solution.cpp
+++ Backtracking/PickNotPick/04. Subsequences with Sum at Most K/solution.cpp
@@ -4,8 +4,32 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> subsequencesWithSumAtMostK(vector<int>& arr, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> subsequencesWithSumAtMostK(vector<int> &arr, int target) {
+    int n = arr.size();
+    vector<vector<int>> res;
+
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+            
+      if (sum > target)
+        return;
+
+      if (idx >= n) {
+        if (sum <= target and !temp.empty()) {
+          res.push_back(temp);
+        }
+        return;
+      }
+
+      sol(temp, idx + 1, sum);
+
+      temp.push_back(arr[idx]);
+      sol(temp, idx + 1, sum + arr[idx]);
+      temp.pop_back();
+    };
+
+    vector<int> temp;
+    sol(temp, 0, 0);
+    return res;
+  }
 };
