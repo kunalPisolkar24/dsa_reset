94d7a181df4cd9afc18d17321238406787aab982
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-03 10:09:57 +0530

    done w/ 4th

--- Backtracking/Loop_N_Choose/04. Generate Subsets Whose Sum <= Target/solution.cpp
+++ Backtracking/Loop_N_Choose/04. Generate Subsets Whose Sum <= Target/solution.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> subsetsWithSumLimit(vector<int>& arr, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> subsetsWithSumLimit(vector<int> &arr, int target) {
+    int n = arr.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+      if (sum > target) {
+        return;
+      }
+      res.push_back(temp);
+      for (int i = idx; i < n; i++) {
+        temp.push_back(arr[i]);
+        sol(temp, i + 1, sum + arr[i]);
+        temp.pop_back();
+      }
+    };
+    sol(temp, 0, 0);
+    return res;
+  }
 };
