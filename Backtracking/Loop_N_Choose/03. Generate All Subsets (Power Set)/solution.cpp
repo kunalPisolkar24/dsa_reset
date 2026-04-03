7632b89083bf9e5044be7c19246d076f93acf9a0
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-03 09:58:43 +0530

    done w/ 3rd

--- Backtracking/Loop_N_Choose/03. Generate All Subsets (Power Set)/solution.cpp
+++ Backtracking/Loop_N_Choose/03. Generate All Subsets (Power Set)/solution.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& arr) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> subsets(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      res.push_back(temp);
+      for (int i = idx; i < n; i++) {
+        temp.push_back(arr[i]);
+        sol(temp, i + 1);
+        temp.pop_back();
+      }
+    };
+
+    sol(temp, 0);
+
+    return res;
+  }
 };
