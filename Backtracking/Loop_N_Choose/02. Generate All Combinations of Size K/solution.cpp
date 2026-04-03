9bf01ae50572c4590dba3e8fd10bd384a98a520f
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-03 09:55:07 +0530

    done w/ 2nd

--- Backtracking/Loop_N_Choose/02. Generate All Combinations of Size K/solution.cpp
+++ Backtracking/Loop_N_Choose/02. Generate All Combinations of Size K/solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combine(vector<int>& arr, int k) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combine(vector<int> &arr, int k) {
+    vector<int> temp;
+    int n = arr.size();
+    vector<vector<int>> res;
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      if (temp.size() == k) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        temp.push_back(arr[i]);
+        sol(temp, i + 1);
+        temp.pop_back();
+      }
+    };
+    sol(temp, 0);
+    return res;
+  }
 };
