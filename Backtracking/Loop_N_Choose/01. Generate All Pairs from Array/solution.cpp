47f2d54e18d719b505f3ce11a7f4f1560b72b05c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-03 09:45:59 +0530

    done w/ 1st

--- Backtracking/Loop_N_Choose/01. Generate All Pairs from Array/solution.cpp
+++ Backtracking/Loop_N_Choose/01. Generate All Pairs from Array/solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> generatePairs(vector<int>& arr) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> generatePairs(vector<int> &arr) {
+    int n = arr.size();
+    vector<vector<int>> res;
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      if (temp.size() == 2) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        temp.push_back(arr[i]);
+        sol(temp, i + 1);
+        temp.pop_back();
+      }
+    };
+    vector<int> temp;
+    sol(temp, 0);
+    return res;
+  }
 };
