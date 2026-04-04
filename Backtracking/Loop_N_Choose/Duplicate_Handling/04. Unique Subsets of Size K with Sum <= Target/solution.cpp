17b0191d5f397489a2243915c5f6d95a390ece2c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-04 13:35:56 +0530

    done w/ 4th

--- Backtracking/Loop_N_Choose/Duplicate_Handling/04. Unique Subsets of Size K with Sum <= Target/solution.cpp
+++ Backtracking/Loop_N_Choose/Duplicate_Handling/04. Unique Subsets of Size K with Sum <= Target/solution.cpp
@@ -4,8 +4,33 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> uniqueSubsetsOfSizeKWithSumLimit(vector<int>& nums, int k, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> uniqueSubsetsOfSizeKWithSumLimit(vector<int> &nums, int k,
+                                                       int target) {
+    int n = nums.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+      if (sum > target or temp.size() > k)
+        return;
+    
+      if (temp.size() == k) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        if (i > idx and nums[i] == nums[i - 1])
+          continue;
+        temp.push_back(nums[i]);
+        sol(temp, i + 1, sum + nums[i]);
+        temp.pop_back();
+      }
+    };
+
+    sol(temp, 0, 0);
+    return res;
+  }
 };
