db89a46b65ccc158909bb0cfea859272a1e4dd99
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-04 13:05:22 +0530

    done w/ 3rd

--- Backtracking/Loop_N_Choose/Duplicate_Handling/03. Combinations of Size K Without Duplicates/solution.cpp
+++ Backtracking/Loop_N_Choose/Duplicate_Handling/03. Combinations of Size K Without Duplicates/solution.cpp
@@ -4,8 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combineUnique(vector<int>& nums, int k) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combineUnique(vector<int> &nums, int k) {
+    int n = nums.size();
+    vector<vector<int>> res;
+    vector<int> temp;
+    sort(nums.begin(), nums.end());
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      if (temp.size() > k)
+        return;
+
+      if (temp.size() == k) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        if (i > idx and nums[i] == nums[i - 1])
+          continue;
+        temp.push_back(nums[i]);
+        sol(temp, i + 1);
+        temp.pop_back();
+      }
+    };
+    sol(temp, 0);
+    return res;
+  }
 };
