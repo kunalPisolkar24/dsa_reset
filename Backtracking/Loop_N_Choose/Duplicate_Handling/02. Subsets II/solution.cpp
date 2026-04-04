d18e85233b83e8b3a68e27330e8aa838a7373d56
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-04 13:00:41 +0530

    done w/ 2nd

--- Backtracking/Loop_N_Choose/Duplicate_Handling/02. Subsets II/solution.cpp
+++ Backtracking/Loop_N_Choose/Duplicate_Handling/02. Subsets II/solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+    int n = nums.size();
+    vector<vector<int>> res;
+    vector<int> temp;
+    sort(nums.begin(), nums.end());
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp,
+                                                 int idx) -> void {
+      res.push_back(temp);
+
+      for (int i = idx; i < n; i++) {
+        if (i > idx and nums[i] == nums[i - 1])
+          continue;
+        temp.push_back(nums[i]);
+        sol(temp, i + 1);
+        temp.pop_back();
+      }
+    };
+    sol(temp, 0);
+    return res;
+  }
 };
