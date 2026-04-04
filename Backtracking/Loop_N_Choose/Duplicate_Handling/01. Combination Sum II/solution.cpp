8899573a5898c8b18c82bbe1cbf57ee1065773d2
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-04 12:56:10 +0530

    done w/ 1st

--- Backtracking/Loop_N_Choose/Duplicate_Handling/01. Combination Sum II/solution.cpp
+++ Backtracking/Loop_N_Choose/Duplicate_Handling/01. Combination Sum II/solution.cpp
@@ -4,8 +4,33 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+    int n = candidates.size();
+
+    vector<int> temp;
+    vector<vector<int>> res;
+    sort(candidates.begin(), candidates.end());
+
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+      if (sum > target)
+        return;
+
+      if (sum == target) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        if (i > idx and candidates[i] == candidates[i - 1])
+          continue;
+        temp.push_back(candidates[i]);
+        sol(temp, i + 1, sum + candidates[i]);
+        temp.pop_back();
+      }
+    };
+
+    sol(temp, 0, 0);
+    return res;
+  }
 };
