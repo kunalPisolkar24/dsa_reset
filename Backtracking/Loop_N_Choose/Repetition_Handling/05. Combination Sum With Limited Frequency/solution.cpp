1ce2f96ef1c9ea3455a0e38a0a534d3240048fc3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-05 11:44:20 +0530

    done w/ 5th

--- Backtracking/Loop_N_Choose/Repetition_Handling/05. Combination Sum With Limited Frequency/solution.cpp
+++ Backtracking/Loop_N_Choose/Repetition_Handling/05. Combination Sum With Limited Frequency/solution.cpp
@@ -4,8 +4,43 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSumFreq(vector<int>& nums, vector<int>& freq, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combinationSumFreq(vector<int> &nums, vector<int> &freq,
+                                         int target) {
+    int n = nums.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+
+      if (sum == target) {
+        res.push_back(temp);
+        return;
+      }
+
+      if (idx == n or sum > target)
+        return;
+
+      for (int i = 0; i <= freq[idx]; i++) {
+        int newTotal = i * nums[idx];
+        int newSum = sum + newTotal;
+
+        if (newSum > target)
+          break;
+
+        for (int j = 0; j < i; j++) {
+          temp.push_back(nums[idx]);
+        }
+
+        sol(temp, idx + 1, newSum);
+
+        for (int j = 0; j < i; j++) {
+          temp.pop_back();
+        }
+      }
+    };
+
+    sol(temp, 0, 0);
+    return res;
+  }
 };
