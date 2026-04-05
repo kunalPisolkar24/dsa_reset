373e1836f5ee5b2e44851f9194e3a5d5e3fe8ff1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-05 10:55:26 +0530

    done w/ 3rd

--- Backtracking/Loop_N_Choose/Repetition_Handling/03. Combination Sum With Size Constraint/solution.cpp
+++ Backtracking/Loop_N_Choose/Repetition_Handling/03. Combination Sum With Size Constraint/solution.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSumK(vector<int>& nums, int target, int k) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combinationSumK(vector<int> &nums, int target, int k) {
+    int n = nums.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+    function<void(vector<int> & temp, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+      if (sum > target or temp.size() > k)
+        return;
+
+      if (temp.size() == k and sum == target) {
+        res.push_back(temp);
+        return;
+      }
+      for (int i = idx; i < n; i++) {
+        temp.push_back(nums[i]);
+        sol(temp, i, sum + nums[i]);
+        temp.pop_back();
+      }
+    };
+    sol(temp, 0, 0);
+    return res;
+  }
 };
