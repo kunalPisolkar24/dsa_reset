7ea1a5fc9dcf68bdb048dfd02051b60f0e07d47d
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-05 10:21:47 +0530

    done w/ 1st

--- Backtracking/Loop_N_Choose/Repetition_Handling/01. Combinations of Size K With Repetition/solution.cpp
+++ Backtracking/Loop_N_Choose/Repetition_Handling/01. Combinations of Size K With Repetition/solution.cpp
@@ -4,8 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combineWithRepetition(vector<int>& nums, int k) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combineWithRepetition(vector<int> &nums, int k) {
+    int n = nums.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+
+    function<void(vector<int> &, int)> sol = [&](vector<int> &temp, int idx) -> void {
+      if(temp.size() > k) return;
+      
+      if (temp.size() == k) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        temp.push_back(nums[i]);
+        sol(temp, i);
+        temp.pop_back();
+      }
+    };
+
+    sol(temp, 0);
+    return res;
+  }
 };
