a07dff865435f7fea9124259b878d94247149d4e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-05 10:41:31 +0530

    done w/ 2nd

--- Backtracking/Loop_N_Choose/Repetition_Handling/02. Combination Sum (Unlimited Usage)/solution.cpp
+++ Backtracking/Loop_N_Choose/Repetition_Handling/02. Combination Sum (Unlimited Usage)/solution.cpp
@@ -4,8 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // Implement your solution here
-        return {};
-    }
+  vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+    int n = candidates.size();
+    vector<int> temp;
+    vector<vector<int>> res;
+
+    function<void(vector<int> &, int, int)> sol =
+        [&](vector<int> &temp, int idx, int sum) -> void {
+      if (sum > target)
+        return;
+
+      if (sum == target) {
+        res.push_back(temp);
+        return;
+      }
+
+      for (int i = idx; i < n; i++) {
+        temp.push_back(candidates[i]);
+        sol(temp, i, sum + candidates[i]);
+        temp.pop_back();
+      }
+    };
+
+    sol(temp, 0, 0);
+
+    return res;
+  }
 };
