8de2d96c676bfe7c45c89618ae2259ec03efc01b
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-07 13:10:59 +0530

    done w/ 2nd

--- Greedy/Sorting/01. Foundations/02. Even-Odd Priority Sort/solution.cpp
+++ Greedy/Sorting/01. Foundations/02. Even-Odd Priority Sort/solution.cpp
@@ -4,8 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> evenOddSort(vector<int> arr) {
-        // Implement even numbers first (ascending), then odd numbers (descending)
-        return {};
-    }
+  vector<int> evenOddSort(vector<int> arr) {
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+
+      if (a % 2 == 0 and b % 2 != 0)
+        return true;
+      else if (a % 2 != 0 and b % 2 == 0)
+        return false;
+
+      if (a % 2 == 0 and b % 2 == 0)
+        return a < b;
+      if (a % 2 != 0 and b % 2 != 0)
+        return a > b;
+
+      return false;
+    });
+    return arr;
+  }
 };
