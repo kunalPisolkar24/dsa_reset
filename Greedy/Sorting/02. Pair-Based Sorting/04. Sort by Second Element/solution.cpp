a1e11d6523f3fd5f3be53ca49c3ad8f30612b5d1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-08 19:59:45 +0530

    done with 1st

--- Greedy/Sorting/02. Pair-Based Sorting/04. Sort by Second Element/solution.cpp
+++ Greedy/Sorting/02. Pair-Based Sorting/04. Sort by Second Element/solution.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 class Solution {
 public:
-    vector<pair<int, int>> sortBySecond(vector<pair<int, int>> arr) {
-        // Implement sort by second element (ascending)
-        // If second elements are equal, sort by first element (ascending)
-        return {};
-    }
+  vector<pair<int, int>> sortBySecond(vector<pair<int, int>> arr) {
+    sort(arr.begin(), arr.end(), [](pair<int, int> &a, pair<int, int> &b) {
+        if(a.second == b.second) {
+            return a.first < b.first;
+        }
+        return a.second < b.second;
+    });
+    return arr;
+  }
 };
