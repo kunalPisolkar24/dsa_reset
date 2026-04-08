2a18d43c2830e36ddb0af606262888e741947e1d
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-08 20:02:44 +0530

    done w/ 2nd

--- Greedy/Sorting/02. Pair-Based Sorting/05. Sort by Difference/solution.cpp
+++ Greedy/Sorting/02. Pair-Based Sorting/05. Sort by Difference/solution.cpp
@@ -5,8 +5,14 @@ using namespace std;
 class Solution {
 public:
     vector<pair<int, int>> sortByDifference(vector<pair<int, int>> arr) {
-        // Implement sort by difference (a - b) ascending
-        // If differences are equal, sort by first element (a) ascending
-        return {};
+        sort(arr.begin(), arr.end(), [](pair<int,int> &a, pair<int,int>&b) {
+            int x = a.first - a.second;
+            int y = b.first - b.second;
+            if(x == y) {
+                return a.first < b.first;
+            }
+            return x < y;
+        });
+        return arr;
     }
 };
