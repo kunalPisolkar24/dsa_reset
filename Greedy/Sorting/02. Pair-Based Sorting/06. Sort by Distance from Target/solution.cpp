64252dffd6b3d4aa0e76637cacdd1149321972d8
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-08 20:11:10 +0530

    done w/ 6th

--- Greedy/Sorting/02. Pair-Based Sorting/06. Sort by Distance from Target/solution.cpp
+++ Greedy/Sorting/02. Pair-Based Sorting/06. Sort by Distance from Target/solution.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> sortByDistance(vector<int> arr, int k) {
-        // Implement sort by absolute distance from k
-        // If distances are equal, smaller element first
-        return {};
-    }
+  vector<int> sortByDistance(vector<int> arr, int k) {
+    sort(arr.begin(), arr.end(), [&](int a, int b) {
+      long long d1 = llabs((long long)a - k);
+      long long d2 = llabs((long long)b - k);
+
+      if (d1 == d2)
+        return a < b;
+      return d1 < d2;
+    });
+    return arr;
+  }
 };
