1d2879662d6526dd5905749aead508f53a404e70
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-10 17:34:39 +0530

    done w/ 1st

--- Dynamic Programming/Linear DP/Fibonacci_style/01. Fibonacci Number/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/01. Fibonacci Number/solution.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    int fib(int n) {
-        // Implementation goes here
-        return 0;
-    }
+  int fib(int N) {
+    vector<int> dp(N + 1, -1);
+    function<int(int)> sol = [&](int n) -> int {
+      if (n <= 1)
+        return n;
+      if (dp[n] != -1)
+        return dp[n];
+      return dp[n] = sol(n - 1) + sol(n - 2);
+    };
+    return sol(N);
+  }
 };
