f1a7c4554a641b36dd287bb7d44df124f9a0b690
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-10 10:00:48 +0530

    done w/ 2nd

--- Greedy/Sorting/03. Thinking-Based Sorting/02. Interval Sorting Study/solution.cpp
+++ Greedy/Sorting/03. Thinking-Based Sorting/02. Interval Sorting Study/solution.cpp
@@ -4,21 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    vector<pair<int, int>> sortByStart(vector<pair<int, int>> intervals) {
-        // Implement sort by start time (ascending)
-        // If starts are equal, sort by end
-        return {};
-    }
+  vector<pair<int, int>> sortByStart(vector<pair<int, int>> intervals) {
+    sort(intervals.begin(), intervals.end(),
+         [](pair<int, int> &a, pair<int, int> &b) {
+           if (a.first == b.first)
+             return a.second < b.second;
+           return a.first < b.first;
+         });
+    return intervals;
+  }
 
-    vector<pair<int, int>> sortByEnd(vector<pair<int, int>> intervals) {
-        // Implement sort by end time (ascending)
-        // If ends are equal, sort by start
-        return {};
-    }
+  vector<pair<int, int>> sortByEnd(vector<pair<int, int>> intervals) {
+    sort(intervals.begin(), intervals.end(),
+         [](pair<int, int> &a, pair<int, int> &b) {
+           if (a.second == b.second)
+             return a.first < b.first;
+           return a.second < b.second;
+         });
+    return intervals;
+  }
 
-    vector<pair<int, int>> sortByLength(vector<pair<int, int>> intervals) {
-        // Implement sort by interval length (ascending)
-        // If lengths are equal, sort by start
-        return {};
-    }
+  vector<pair<int, int>> sortByLength(vector<pair<int, int>> intervals) {
+    sort(intervals.begin(), intervals.end(),
+         [](pair<int, int> &a, pair<int, int> &b) {
+           int x = abs(a.second - a.first), y = abs(b.second - b.first);
+           if (x == y)
+             return a.first < b.first;
+           return x < y;
+         });
+    return intervals;
+  }
 };
