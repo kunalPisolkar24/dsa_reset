69f705698db645be6cd275bf2f0d0c8c2f4242e7
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-10 10:26:19 +0530

    done with 3rd

--- Greedy/Sorting/03. Thinking-Based Sorting/03. Stability Observation/solution.cpp
+++ Greedy/Sorting/03. Thinking-Based Sorting/03. Stability Observation/solution.cpp
@@ -1,12 +1,15 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<pair<int, char>> stableSortByValue(vector<pair<int, char>> arr) {
-        // Implement a stable sort by value (ascending)
-        // Ensure relative order of labels for equal values is preserved
-        return {};
-    }
+  vector<pair<int, char>> stableSortByValue(vector<pair<int, char>> arr) {
+    stable_sort(arr.begin(), arr.end(),
+                [](const pair<int, char> &a, const pair<int, char> &b) {
+                  return a.first < b.first;
+                });
+    return arr;
+  }
 };
