edf8feb0213b9a79d73f3e0662ebed5bb4bafc6c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-10 18:24:12 +0530

    done w/ 5th

--- Dynamic Programming/Linear DP/Fibonacci_style/05. Climbing Stairs (Even Steps Only)/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/05. Climbing Stairs (Even Steps Only)/solution.cpp
@@ -4,8 +4,28 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        // Implementation goes here
+  int climbStairs(int N) {
+    if (N & 1)
+      return 0;
+    vector<int> dp(N + 1, -1);
+    int MOD = 1e9 + 7;
+
+    function<int(int)> sol = [&](int n) -> int {
+      if (n < 0)
         return 0;
-    }
+      if (n == 0)
+        return 1;
+
+      if (dp[n] != -1)
+        return dp[n];
+
+      long long sum = 0;
+      sum = (sum + sol(n - 2)) % MOD;
+      sum = (sum + sol(n - 4)) % MOD;
+
+      return dp[n] = sum;
+    };
+
+    return sol(N);
+  }
 };
