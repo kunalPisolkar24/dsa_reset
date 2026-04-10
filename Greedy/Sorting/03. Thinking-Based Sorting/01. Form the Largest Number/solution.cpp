af6c45df16f3a2f8036e16870470bf4a49bb1132
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-10 09:55:54 +0530

    done w/ 1st

--- Greedy/Sorting/03. Thinking-Based Sorting/01. Form the Largest Number/solution.cpp
+++ Greedy/Sorting/03. Thinking-Based Sorting/01. Form the Largest Number/solution.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    string largestNumber(vector<int> arr) {
-        return "";
+  string largestNumber(vector<int> arr) {
+    sort(arr.begin(), arr.end(), [](int &a, int &b) {
+      string x = to_string(a), y = to_string(b);
+      if (x == y)
+        return x.length() < y.length();
+      return x + y > y + x;
+    });
+    if (arr[0] == 0) return "0";
+    string res;
+    for (auto i : arr) {
+      res += to_string(i);
     }
+    return res;
+  }
 };
