3a022568f9458ef1c7d483930607a9592809e6cc
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-12 06:05:42 +0530

    approach 2

--- Dynamic Programming/Linear DP/Fibonacci_style/01. Fibonacci Number/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/01. Fibonacci Number/solution.cpp
@@ -6,10 +6,12 @@ class Solution {
 public:
   int fib(int n) {
     if(n == 0) return 0;
-    vector<int> dp(n + 1, 0);
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++)
-      dp[i] = dp[i - 1] + dp[i - 2];
-    return dp[n];
+    int prev1 = 0, prev2 = 1;
+    for(int i = 2; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    return prev2;
   }
 };
