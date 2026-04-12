684653bc121cdf6bd6e10c48097a2c42a48ff0a3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-12 06:22:37 +0530

    done w/ 3rd

--- Dynamic Programming/Linear DP/Fibonacci_style/03. Climbing Stairs (1, 2, 3 steps)/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/03. Climbing Stairs (1, 2, 3 steps)/solution.cpp
@@ -11,11 +11,13 @@ public:
     if (n == 2)
       return 2;
 
-    vector<int> dp(n + 1, 0);
-    dp[0] = dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++)
-      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    return dp[n];
+    int p1 = 1, p2 = 1, p3 = 2;
+    for (int i = 3; i <= n; i++) {
+      int curr = p1 + p2 + p3;
+      p1 = p2;
+      p2 = p3;
+      p3 = curr;
+    }
+    return p3;
   }
 };
