1d27a35014b43dfe92b7cf91cfb9c69b9e55fc70
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-12 07:23:30 +0530

    approach 1 for 5th

--- Dynamic Programming/Linear DP/Fibonacci_style/05. Climbing Stairs (Even Steps Only)/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/05. Climbing Stairs (Even Steps Only)/solution.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        // Implementation goes here
-        return 0;
+  int climbStairs(int n) {
+    if (n & 1)
+      return 0;
+
+    int MOD = 1e9 + 7;
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    if (n >= 2)
+      dp[2] = 1;
+    if (n >= 4)
+      dp[4] = 2;
+
+    for (int i = 6; i <= n; i += 2) {
+      dp[i] = (dp[i - 2] + dp[i - 4]) % MOD;
     }
+    return dp[n];
+  }
 };
