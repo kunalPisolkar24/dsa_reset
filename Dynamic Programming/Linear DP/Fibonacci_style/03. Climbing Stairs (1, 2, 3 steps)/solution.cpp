b1e37861b73712f4679e86078f110b0bda34e1e1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-12 06:21:44 +0530

    approahc 1

--- Dynamic Programming/Linear DP/Fibonacci_style/03. Climbing Stairs (1, 2, 3 steps)/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/03. Climbing Stairs (1, 2, 3 steps)/solution.cpp
@@ -5,12 +5,12 @@ using namespace std;
 class Solution {
 public:
   int climbStairs(int n) {
-    if (n == 1 or n == 2)
+    if (n == 1 or n == 0)
       return 1;
 
     if (n == 2)
       return 2;
-    
+
     vector<int> dp(n + 1, 0);
     dp[0] = dp[1] = 1;
     dp[2] = 2;
