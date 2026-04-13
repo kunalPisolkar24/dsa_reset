ce479aa8b42d04356289b9baab40e094b4bb5d08
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-13 18:26:54 +0530

    add approach 1 for 9th

--- Dynamic Programming/Linear DP/Fibonacci_style/09. Min Cost Climbing Stairs/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/09. Min Cost Climbing Stairs/solution.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        // Implementation goes here
-        return 0;
+  int minCostClimbingStairs(vector<int> &cost) {
+    int n = cost.size();
+    vector<int> dp(n + 1, 0);
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 2; i <= n; i++) {
+      int jump1 = dp[i - 1] + cost[i - 1];
+      int jump2 = dp[i - 2] + cost[i - 2];
+      dp[i] = min(jump1, jump2);
     }
+    return dp[n];
+  }
 };
