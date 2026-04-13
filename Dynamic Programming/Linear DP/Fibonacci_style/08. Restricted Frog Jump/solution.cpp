6c7fc8238d13e5a8b6e5850e9f8b9a25ca6baf55
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-13 10:13:43 +0530

    add approach 1 for 8th

--- Dynamic Programming/Linear DP/Fibonacci_style/08. Restricted Frog Jump/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/08. Restricted Frog Jump/solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int minCost(vector<int>& heights) {
-        // Implementation goes here
-        return 0;
+  int minCost(vector<int> &heights) {
+    int n = heights.size();
+    vector<int> dp(n, 0);
+    dp[0] = 0;
+    dp[1] = abs(heights[1] - heights[0]);
+    for (int i = 2; i < n; i++) {
+      int jump1 = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+      int jump2 = INT_MAX;
+      if (i & 1) {
+        jump2 = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+      }
+      dp[i] = min(jump1, jump2);
     }
+    return dp[n - 1];
+  }
 };
