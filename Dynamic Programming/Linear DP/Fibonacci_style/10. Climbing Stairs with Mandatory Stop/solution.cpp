f37141f00348b88ee6281ea6fb334ce17f53a790
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-13 18:46:52 +0530

    approach 1 for 10th

--- Dynamic Programming/Linear DP/Fibonacci_style/10. Climbing Stairs with Mandatory Stop/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/10. Climbing Stairs with Mandatory Stop/solution.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairsWithMandatoryStop(int n, int m) {
-        // Implementation goes here
-        return 0;
-    }
+  int climbStairsWithMandatoryStop(int n, int m) {
+    int MOD = 1e9 + 7;
+    auto sol = [&](int n) -> int {
+      if (n == 1 or n == 0)
+        return 1;
+      vector<int> dp(n + 1, 0);
+      dp[0] = dp[1] = 1;
+      for (int i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+      }
+      return dp[n];
+    };
+    return (1LL * sol(m) * sol(n - m)) % MOD;
+  }
 };
