eb70c05e3377d3f40294a9aac43cd5e662d0cc0f
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-13 09:58:16 +0530

    approach 1st for 7th

--- Dynamic Programming/Linear DP/Fibonacci_style/07. Frog Jump/solution.cpp
+++ Dynamic Programming/Linear DP/Fibonacci_style/07. Frog Jump/solution.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    int minCost(vector<int>& heights) {
-        // Implementation goes here
-        return 0;
+  int minCost(vector<int> &heights) {
+    int n = heights.size();
+    vector<int> dp(n + 1, 0);
+    dp[0] = 0;
+    dp[1] = abs(heights[1] - heights[0]);
+    for (int i = 2; i < n; i++) {
+      dp[i] = min(dp[i - 1] + abs(heights[i] - heights[i - 1]),
+                  dp[i - 2] + abs(heights[i] - heights[i - 2]));
     }
+    return dp[n - 1];
+  }
 };
