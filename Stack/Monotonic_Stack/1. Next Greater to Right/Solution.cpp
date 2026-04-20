133b3fdcfc9f007c3fac7794ba7c75f779b7d383
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-20 15:45:15 +0530

    done w/ 1st

--- Stack/Monotonic_Stack/1. Next Greater to Right/Solution.cpp
+++ Stack/Monotonic_Stack/1. Next Greater to Right/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> nextGreaterRight(vector<int>& arr) {
-        // Your code here
-        return {};
+  vector<int> nextGreaterRight(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> res(n, 0);
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--) {
+      while (!st.empty() and st.top() <= arr[i]) {
+        st.pop();
+      }
+      res[i] = st.empty() ? -1 : st.top();
+      st.push(arr[i]);
     }
+    return res;
+  }
 };
