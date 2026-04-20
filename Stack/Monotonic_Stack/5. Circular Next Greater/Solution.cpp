a20c8b4e9166e0d67a799a1da6589c83affdbb25
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-20 16:19:45 +0530

    done w/ 5th

--- Stack/Monotonic_Stack/5. Circular Next Greater/Solution.cpp
+++ Stack/Monotonic_Stack/5. Circular Next Greater/Solution.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> nextGreaterCircular(vector<int>& arr) {
-        // Your code here
-        return {};
+  vector<int> nextGreaterCircular(vector<int> &arr) {
+    int n = arr.size();
+    stack<int> st;
+    vector<int> res(n);
+    for (int i = 2 * n - 1; i >= 0; i--) {
+      while (!st.empty() and st.top() <= arr[i % n]) {
+        st.pop();
+      }
+      if (i < n)
+        res[i % n] = st.empty() ? -1 : st.top();
+      st.push(arr[i % n]);
     }
+    return res;
+  }
 };
