8ec18e5bcbb35f1ca6b9a68cbbd2a232ab6396af
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-20 19:41:21 +0530

    done w/ 6th

--- Stack/Monotonic_Stack/6. Daily Temperature Variant/Solution.cpp
+++ Stack/Monotonic_Stack/6. Daily Temperature Variant/Solution.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> dailyTempVariant(vector<int>& temps) {
-        // Your code here
-        return {};
+  vector<int> dailyTempVariant(vector<int> &temps) {
+    int n = temps.size();
+    vector<int> res(n, 0);
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--) {
+      int target = temps[i] + 3;
+      while (!st.empty() and temps[st.top()] < target) {
+        st.pop();
+      }
+
+      res[i] = st.empty() ? -1 : (st.top() - i);
+      st.push(i);
     }
+    return res;
+  }
 };
