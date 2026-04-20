8833cf0705554dfc96a249cfc00538724b2242ff
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-20 15:57:06 +0530

    done w/ 3rd

--- Stack/Monotonic_Stack/3. Previous Greater Element/Solution.cpp
+++ Stack/Monotonic_Stack/3. Previous Greater Element/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> previousGreater(vector<int>& arr) {
-        // Your code here
-        return {};
+  vector<int> previousGreater(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> res(n);
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+      while (!st.empty() and st.top() <= arr[i])
+        st.pop();
+
+      res[i] = st.empty() ? -1 : st.top();
+      st.push(arr[i]);
     }
+    return res;
+  }
 };
