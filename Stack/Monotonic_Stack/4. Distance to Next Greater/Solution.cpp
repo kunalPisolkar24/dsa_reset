735a7752b6af283176f28a52e519558e61488e78
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-20 16:04:08 +0530

    done w/ 4th

--- Stack/Monotonic_Stack/4. Distance to Next Greater/Solution.cpp
+++ Stack/Monotonic_Stack/4. Distance to Next Greater/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> distanceToNextGreater(vector<int>& arr) {
-        // Your code here
-        return {};
+  vector<int> distanceToNextGreater(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> res(n);
+    stack<pair<int, int>> st;
+    for (int i = n - 1; i >= 0; i--) {
+      while (!st.empty() and st.top().first <= arr[i]) {
+        st.pop();
+      }
+      res[i] = st.empty() ? -1 : (st.top().second - i);
+      st.push({arr[i], i});
     }
+    return res;
+  }
 };
