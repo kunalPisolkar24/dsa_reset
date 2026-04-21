cf207b0bb8f8e3d15777ab13615ded6a5636670c
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-21 17:06:30 +0530

    done w/ 10th

--- Stack/Monotonic_Stack/10. Largest Rectangle in Histogram/Solution.cpp
+++ Stack/Monotonic_Stack/10. Largest Rectangle in Histogram/Solution.cpp
@@ -7,38 +7,19 @@ public:
   int largestRectangleArea(vector<int> &heights) {
     long long mx = 0;
     int n = heights.size();
-    auto smallestOnLeft = [&]() -> vector<int> {
-      vector<int> res(n, 0);
-      stack<int> st;
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() and heights[st.top()] >= heights[i]) {
-          st.pop();
-        }
-        res[i] = st.empty() ? -1 : st.top();
-        st.push(i);
+    stack<int> st;
+    for (int i = 0; i <= n; i++) {
+      int curr = (i == n ? 0 : heights[i]);
+      while (!st.empty() and heights[st.top()] > curr) {
+        int height = heights[st.top()];
+        st.pop();
+        int left = st.empty() ? -1 : st.top();
+        int right = i;
+        long long width = right - left - 1;
+        mx = max(mx, 1LL * height * width);
       }
-      return res;
-    };
-
-    auto smallestOnRight = [&]() -> vector<int> {
-      vector<int> res(n, 0);
-      stack<int> st;
-      for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() and heights[st.top()] >= heights[i]) {
-          st.pop();
-        }
-        res[i] = st.empty() ? n : st.top();
-        st.push(i);
-      }
-      return res;
-    };
-
-    vector<int> left = smallestOnLeft(), right = smallestOnRight();
-
-    for (int i = 0; i < n; i++) {
-        int width = right[i] - left[i] - 1;
-        mx = max(mx, (long long) heights[i] * width);
+      st.push(i);
     }
-    return mx;
+    return (int)mx;
   }
 };
