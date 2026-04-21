4a58e3bb1c7072b2d5bc54020fddad556284cfba
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-21 12:17:01 +0530

    done w/ 7th

--- Stack/Monotonic_Stack/7. Stock Span Variant/Solution.cpp
+++ Stack/Monotonic_Stack/7. Stock Span Variant/Solution.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> stockSpan(vector<int>& prices) {
-        // Your code here
-        return {};
+  vector<int> stockSpan(vector<int> &prices) {
+    int n = prices.size();
+    vector<int> res(n, 0);
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+      while (!st.empty() and prices[st.top()] <= prices[i]) {
+        st.pop();
+      }
+      res[i] = st.empty() ? i + 1 : (i - st.top());
+      st.push(i);
     }
+    return res;
+  }
 };
