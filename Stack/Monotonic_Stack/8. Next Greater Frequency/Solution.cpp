9b27139d9c95065c275937b473ac9193ad933916
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-21 12:40:14 +0530

    done w/ 8th

--- Stack/Monotonic_Stack/8. Next Greater Frequency/Solution.cpp
+++ Stack/Monotonic_Stack/8. Next Greater Frequency/Solution.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> nextGreaterFrequency(vector<int>& arr) {
-        // Your code here
-        return {};
+  vector<int> nextGreaterFrequency(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> res(n);
+    // index of element
+    stack<int> st;
+
+    unordered_map<int, int> ump;
+    for (auto i : arr) {
+      ump[i]++;
     }
+
+    for (int i = n - 1; i >= 0; i--) {
+      while (!st.empty() and ump[arr[st.top()]] <= ump[arr[i]]) {
+        st.pop();
+      }
+      res[i] = st.empty() ? -1 : arr[st.top()];
+      st.push(i);
+    }
+    return res;
+  }
 };
