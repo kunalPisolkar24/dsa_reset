9572fd8b197c8302ae4467d86084bc2fe8d7c0a5
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-21 16:02:56 +0530

    done w/ 9th

--- Stack/Monotonic_Stack/9. First Taller Building/Solution.cpp
+++ Stack/Monotonic_Stack/9. First Taller Building/Solution.cpp
@@ -5,7 +5,16 @@ using namespace std;
 class Solution {
 public:
     vector<int> firstTallerBuilding(vector<int>& heights) {
-        // Your code here
-        return {};
+        int n = heights.size();
+        vector<int> res(n, 0);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() and st.top() <= heights[i]) {
+                st.pop();
+            }
+            res[i] = st.empty() ? -1 : st.top();
+            st.push(heights[i]);
+        }
+        return res;
     }
 };
