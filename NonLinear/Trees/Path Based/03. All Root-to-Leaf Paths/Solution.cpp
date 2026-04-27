94a1e6a740e00d1cfa232772105b94883215a8e1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-27 18:48:03 +0530

    approach one

--- NonLinear/Trees/Path Based/03. All Root-to-Leaf Paths/Solution.cpp
+++ NonLinear/Trees/Path Based/03. All Root-to-Leaf Paths/Solution.cpp
@@ -2,15 +2,42 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        // Implementation here
-        return {};
-    }
+  vector<string> binaryTreePaths(TreeNode *root) {
+    vector<string> ans;
+    vector<int> temp;
+    function<void(TreeNode *)> sol = [&](TreeNode *node) -> void {
+      if (!node)
+        return;
+
+      temp.push_back(node->val);
+
+      if (!node->left and !node->right) {
+        string tmp = "";
+        for (int i = 0; i < temp.size(); i++) {
+          tmp += to_string(temp[i]);
+          if (i != temp.size() - 1) {
+            tmp += "->";
+          }
+        }
+        ans.push_back(tmp);
+        temp.pop_back();
+        return;
+      }
+
+      sol(node->left);
+
+      sol(node->right);
+      temp.pop_back();
+    };
+
+    sol(root);
+    return ans;
+  }
 };
