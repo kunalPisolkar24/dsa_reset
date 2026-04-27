ff6387e9ce1af85d79a74365ae84d47bf13daa89
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-27 20:31:37 +0530

    approach 1

--- NonLinear/Trees/Path Based/06. Maximum Root-to-Leaf Path/Solution.cpp
+++ NonLinear/Trees/Path Based/06. Maximum Root-to-Leaf Path/Solution.cpp
@@ -2,15 +2,39 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    vector<int> maxPath(TreeNode* root) {
-        // Implementation here
-        return {};
-    }
+  vector<int> maxPath(TreeNode *root) {
+    int mx = INT_MIN;
+    vector<int> best;
+    vector<int> temp;
+
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int sum) -> void {
+      if (!node)
+        return;
+
+      temp.push_back(node->val);
+
+      if (!node->left and !node->right) {
+        if (sum > mx) {
+          mx = sum;
+          best = temp;
+        }
+        temp.pop_back();
+        return;
+      } else {
+        sol(node->left, sum + (node->left ? node->left->val : 0));
+        sol(node->right, sum + (node->right ? node->right->val : 0));
+      }
+      temp.pop_back();
+    };
+
+    sol(root, root->val);
+    return best;
+  }
 };
