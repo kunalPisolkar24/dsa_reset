ab10c04cb0166b6f0a258cf9a5be038006c2434e
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-27 21:29:10 +0530

    approach 1

--- NonLinear/Trees/Path Based/07. Path Sum III/Solution.cpp
+++ NonLinear/Trees/Path Based/07. Path Sum III/Solution.cpp
@@ -2,15 +2,36 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        // Implementation here
-        return 0;
-    }
+  int pathSum(TreeNode *root, int targetSum) {
+    int count = 0;
+
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int sum) -> void {
+      if (!node)
+        return;
+
+      if (sum - node->val == 0) {
+        count++;
+      }
+      sol(node->left, sum - node->val);
+      sol(node->right, sum - node->val);
+    };
+
+    function<void(TreeNode *)> sol1 = [&](TreeNode *node) -> void {
+      if (!node)
+        return;
+      sol(node, targetSum);
+      sol1(node->left);
+      sol1(node->right);
+    };
+
+    sol1(root);
+    return count;
+  }
 };
