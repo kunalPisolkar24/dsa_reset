fcd7c2a2b85c11b862304b4b1241bc7a172edb8d
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-27 19:16:29 +0530

    approach 1

--- NonLinear/Trees/Path Based/05. Count Root-to-Leaf Paths with Sum/Solution.cpp
+++ NonLinear/Trees/Path Based/05. Count Root-to-Leaf Paths with Sum/Solution.cpp
@@ -2,15 +2,31 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    int countPathSum(TreeNode* root, int targetSum) {
-        // Implementation here
-        return 0;
-    }
+  int countPathSum(TreeNode *root, int targetSum) {
+    int count = 0;
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int sum) -> void {
+      if (!node)
+        return;
+
+      if (!node->left and !node->right) {
+        if (sum - node->val == 0) {
+          count++;
+          return;
+        }
+      } else {
+        sol(node->left, sum - node->val);
+        sol(node->right, sum - node->val);
+      }
+    };
+    
+    sol(root, targetSum);
+    return count;
+  }
 };
