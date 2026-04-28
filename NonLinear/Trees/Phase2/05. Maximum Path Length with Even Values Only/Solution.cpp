ddafb8536e20e499aa3d0e8833fa7f685f95cae1
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 18:57:28 +0530

    approach 1

--- NonLinear/Trees/Phase2/05. Maximum Path Length with Even Values Only/Solution.cpp
+++ NonLinear/Trees/Phase2/05. Maximum Path Length with Even Values Only/Solution.cpp
@@ -3,17 +3,42 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int maxEvenPathLength(TreeNode* root) {
-        return {};
-    }
+  int maxEvenPathLength(TreeNode *root) {
+    int mx = INT_MIN;
+
+    if(!root or root->val % 2 != 0) return 0;
+    
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int lev) -> void {
+      if (!node)
+        return;
+
+      lev += 1;
+
+      if (!node->left and !node->right and node->val % 2 == 0) {
+        mx = max(mx, lev);
+        return;
+      }
+
+      if (node->left and node->left->val % 2 == 0) {
+        sol(node->left, lev);
+      }
+
+      if (node->right and node->right->val % 2 == 0) {
+        sol(node->right, lev);
+      }
+    };
+    sol(root, 0);
+    return mx;
+  }
 };
