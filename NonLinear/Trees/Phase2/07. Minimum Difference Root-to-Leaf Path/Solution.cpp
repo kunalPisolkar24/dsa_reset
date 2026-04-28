93ad5534d69ff8352808b9d813b92173ecffe319
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 21:09:33 +0530

    approach 1

--- NonLinear/Trees/Phase2/07. Minimum Difference Root-to-Leaf Path/Solution.cpp
+++ NonLinear/Trees/Phase2/07. Minimum Difference Root-to-Leaf Path/Solution.cpp
@@ -3,17 +3,35 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int minDiffPath(TreeNode* root) {
-        return {};
-    }
+  int minDiffPath(TreeNode *root) {
+    int global_min = INT_MAX;
+    function<void(TreeNode *, int, int)> sol = [&](TreeNode *node, int mx,
+                                                   int mn) -> void {
+      if (!node)
+        return;
+
+      mx = max(mx, node->val);
+      mn = min(mn, node->val);
+
+      if (!node->left and !node->right) {
+        global_min = min(global_min, mx - mn);
+        return;
+      }
+      sol(node->left, mx, mn);
+      sol(node->right, mx, mn);
+    };
+    sol(root, INT_MIN, INT_MAX);
+    return global_min;
+  }
 };
