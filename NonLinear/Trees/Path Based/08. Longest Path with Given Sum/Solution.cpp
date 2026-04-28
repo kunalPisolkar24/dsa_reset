d31fa445af1527dc2d4ef7d87da4eeda07beb85a
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 11:45:54 +0530

    approach 2

--- NonLinear/Trees/Path Based/08. Longest Path with Given Sum/Solution.cpp
+++ NonLinear/Trees/Path Based/08. Longest Path with Given Sum/Solution.cpp
@@ -12,31 +12,33 @@ public:
   int longestPathWithSum(TreeNode *root, int targetSum) {
 
     int mx = 0;
+    unordered_map<long long, int> ump;
+    ump[0] = 0;
 
-    function<void(TreeNode *, int, int)> sol2 = [&](TreeNode *node, int sum,
-                                                    int count) -> void {
+    function<void(TreeNode *, int, int)> sol = [&](TreeNode *node, int sum,
+                                                   int lev) -> void {
       if (!node)
         return;
-      sum += node->val;
-      count += 1;
 
-      if (sum == targetSum) {
-        mx = max(mx, count);
+      sum += node->val;
+
+      if(ump.count(sum - targetSum)) {
+        mx = max(mx, lev - ump[sum - targetSum]);
       }
 
-      sol2(node->left, sum, count);
-      sol2(node->right, sum, count);
+      if(!ump.count(sum)) {
+        ump[sum] = lev;
+      }
+
+      sol(node->left, sum, lev + 1);
+      sol(node->right, sum, lev + 1);
+
+      if(ump[sum] == lev) {
+        ump.erase(sum);
+      }
     };
 
-    function<void(TreeNode *)> sol = [&](TreeNode *node) -> void {
-      if (!node)
-        return;
-      sol2(node, 0, 0);
-      sol(node->left);
-      sol(node->right);
-    };
-
-    sol(root);
+    sol(root, 0, 1);
     return mx;
   }
 };
