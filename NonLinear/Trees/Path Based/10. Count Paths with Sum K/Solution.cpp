a30ed61750e71eb9812ae175e8a605cd50a1247b
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 12:10:38 +0530

    done w/ 10th

--- NonLinear/Trees/Path Based/10. Count Paths with Sum K/Solution.cpp
+++ NonLinear/Trees/Path Based/10. Count Paths with Sum K/Solution.cpp
@@ -2,15 +2,33 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    int countPathsWithSumK(TreeNode* root, int k) {
-        // Implementation here
-        return 0;
-    }
+  int countPathsWithSumK(TreeNode *root, int k) {
+    unordered_map<long long, int> ump;
+    ump[0] = 1;
+
+    long long ans = 0;
+
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int sum) -> void {
+      if (!node)
+        return;
+      sum += node->val;
+      if (ump.count(sum - k)) {
+        ans += ump[sum - k];
+      }
+
+      ump[sum]++;
+      sol(node->left, sum);
+      sol(node->right, sum);
+      ump[sum]--;
+    };
+    sol(root, 0);
+    return (int)ans;
+  }
 };
