8f22a4a6085d605eb4346263c4dbc350babf6020
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 19:24:57 +0530

    done w/ 6th

--- NonLinear/Trees/Phase2/06. Minimum Depth to a Node with Value >= X/Solution.cpp
+++ NonLinear/Trees/Phase2/06. Minimum Depth to a Node with Value >= X/Solution.cpp
@@ -3,17 +3,34 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-    int minDepthGE(TreeNode* root, int x) {
-        return {};
-    }
+  int minDepthGE(TreeNode *root, int x) {
+    function<int(TreeNode *)> sol = [&](TreeNode *node) -> int {
+      if (!node)
+        return INT_MAX;
+
+      if (node->val >= x)
+        return 1;
+
+      int left = sol(node->left);
+      int right = sol(node->right);
+
+      int best = min(left, right);
+      if (best == INT_MAX)
+        return INT_MAX;
+      return best + 1;
+    };
+    int mn = sol(root);
+    return (mn == INT_MAX ? -1 : mn);
+  }
 };
