e829f7001c3b0c6a5926c89cfa7e0b36e83d83c2
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-04-28 11:54:20 +0530

    approahc 1

--- NonLinear/Trees/Path Based/09. Path with Maximum Even Sum/Solution.cpp
+++ NonLinear/Trees/Path Based/09. Path with Maximum Even Sum/Solution.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 struct TreeNode {
-    int val;
-    TreeNode *left, *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-    int maxEvenPathSum(TreeNode* root) {
-        // Implementation here
-        return -1;
-    }
+  int maxEvenPathSum(TreeNode *root) {
+    int mx = INT_MIN;
+    function<void(TreeNode *, int)> sol = [&](TreeNode *node, int sum) -> void {
+      if (!node)
+        return;
+      sum += node->val;
+      if (!node->left and !node->right) {
+        if ((sum & 1) == 0 and sum > mx) {
+          mx = sum;
+        }
+        return;
+      } else {
+        sol(node->left, sum);
+        sol(node->right, sum);
+      }
+    };
+    sol(root, 0);
+    return mx == INT_MIN ? -1 : mx;
+  }
 };
