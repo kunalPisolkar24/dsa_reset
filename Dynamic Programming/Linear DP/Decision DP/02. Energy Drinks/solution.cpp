98f179edb259140c9aff968f96037699d6f34f47
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-05-08 23:22:53 +0530

    approahc 2

--- Dynamic Programming/Linear DP/Decision DP/02. Energy Drinks/solution.cpp
+++ Dynamic Programming/Linear DP/Decision DP/02. Energy Drinks/solution.cpp
@@ -13,16 +13,14 @@ class Solution {
 public:
   long long maxEnergy(vector<int> &energy) {
     int n = energy.size();
-    vector<ll> dp(n + 1, -1);
-    function<ll(int)> sol = [&](int idx) -> ll {
-      if (idx >= n)
-        return 0;
-      if (dp[idx] != -1)
-        return dp[idx];
-      long long skip = sol(idx + 1);
-      long long choose = energy[idx] + sol(idx + 2);
-      return dp[idx] = max(choose, skip);
-    };
-    return sol(0);
+    vector<ll> dp(n + 1, 0);
+    if (n == 1)
+      return energy[0];
+    dp[0] = energy[0];
+    dp[1] = max(energy[0], energy[1]);
+    for (int i = 2; i < n; i++) {
+      dp[i] = max(dp[i - 1], dp[i - 2] + energy[i]);
+    }
+    return dp[n - 1];
   }
 };
