0c076b7d1a03b270cb80c5219f2440e014571ec3
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-05-08 23:06:14 +0530

    app 3

--- Dynamic Programming/Linear DP/Decision DP/01. Alternate Treasure Path/solution.cpp
+++ Dynamic Programming/Linear DP/Decision DP/01. Alternate Treasure Path/solution.cpp
@@ -17,17 +17,13 @@ public:
     int n = coins.size();
     if (n == 1)
       return coins[0];
-    vector<long long> dp(n + 1, 0);
-    dp[0] = coins[0];
-    dp[1] = max(coins[0], coins[1]);
+    long long p0 = coins[0];
+    long long p1 = max(coins[0], coins[1]);
     for (int i = 2; i < n; i++) {
-      long long skip = dp[i - 1];
-      long long choose = INT_MIN;
-      if (i - 2 >= 0) {
-        choose = dp[i - 2] + coins[i];
-      }
-      dp[i] = max(skip, choose);
+      long long curr = max(p1, p0 + coins[i]);
+      p0 = p1;
+      p1 = curr;
     }
-    return dp[n - 1];
+    return p1;
   }
 };
