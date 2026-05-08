ba59e8bdf6613415c5c419bae3d00e1d2b5a7a44
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-05-08 23:31:59 +0530

    done w/ 3rd

--- Dynamic Programming/Linear DP/Decision DP/03. Museum Alarm System/solution.cpp
+++ Dynamic Programming/Linear DP/Decision DP/03. Museum Alarm System/solution.cpp
@@ -4,16 +4,27 @@ using namespace std;
 
 /**
  * Problem 3: Museum Alarm System
- * 
+ *
  * Cannot rob two adjacent rooms. Can skip ANY number of rooms.
  * Find max loot.
- * 
+ *
  * Goal: Solve in 4 ways (Recursive, Memoized, Tabulated, Space Optimized).
  */
+typedef long long ll;
 class Solution {
 public:
-    long long maxLoot(vector<int>& value) {
-        // Implement your solution here (try all 4 ways!)
-        return 0;
+  long long maxLoot(vector<int> &value) {
+
+    int n = value.size();
+    if (n == 1)
+      return value[0];
+    ll p0 = value[0];
+    ll p1 = max(value[0], value[1]);
+    for (int i = 2; i < n; i++) {
+      long long curr = max(p1, p0 + value[i]);
+      p0 = p1;
+      p1 = curr;
     }
+    return p1;
+  }
 };
